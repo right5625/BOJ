fe445840f8e31362b94390f131a0180147223fa1
전진호 <right5625@naver.com>
2023-02-21 16:58:15 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,5 +1,15 @@
-N = int(input())
-DP = [i for i in range(N + 1)]
-for i in range(4, N + 1):
-    DP[i] = (DP[i - 1] + DP[i - 2]) % 15746
-print(DP[N])
+n = int(input())
+a = sorted(list(map(int, input().split())))
+x = int(input())
+i = 0
+j = n - 1
+cnt = 0
+while i < j:
+    if a[i] + a[j] > x:
+        j -= 1
+    elif a[i] + a[j] < x:
+        i += 1
+    else:
+        cnt += 1
+        j -= 1
+print(cnt)
