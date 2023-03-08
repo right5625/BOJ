1efd82c77c6db276a6817f5db9272e49104ac81a
전진호 <right5625@naver.com>
2023-03-08 22:49:03 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,10 +1,2 @@
 S = input()
-B = input()
-length = len(B)
-stack = []
-for i in S:
-    stack.append(i)
-    while ''.join(stack[-length:]) == B:
-        for _ in range(length):
-            stack.pop()
-print(''.join(stack) if stack else 'FRULA')
+print(S[int(input()) - 1])
