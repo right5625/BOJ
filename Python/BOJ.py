36f14aa974a8fbf6d3172ea53b4d7f34ba2c338b
전진호 <right5625@naver.com>
2023-03-22 11:33:05 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,11 +1,29 @@
 import sys
+from collections import deque
 input = lambda : sys.stdin.readline().rstrip()
 
-cs = 1
-while True:
-    S = input()
-    if S == '0':
-        break
-    r, w, l = map(int, S.split())
-    print(f'Pizza {cs} does not fit on the table.' if r * 2 < (w ** 2 + l ** 2) ** 0.5 else f'Pizza {cs} fits on the table.')
-    cs += 1
+N, K = map(int, input().split())
+vst = [False] * 100001
+q = deque([[N]])
+vst[N] = True
+res = 0
+while q:
+    v = q.popleft()
+    l = []
+    res += 1
+    for i in v:
+        if i == K:
+            l.clear()
+            break
+        if i > 0 and not vst[i - 1]:
+            l.append(i - 1)
+            vst[i - 1] = True
+        if i < 100000 and not vst[i + 1]:
+            l.append(i + 1)
+            vst[i + 1] = True
+        if i * 2 < 100001 and not vst[i * 2]:
+            l.append(i * 2)
+            vst[i * 2] = True
+    if l:
+        q.append(l)
+print(res - 1)
