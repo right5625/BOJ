e0288f625341e66ba3e42c38d1021df8ee7e93aa
전진호 <right5625@naver.com>
2023-05-11 11:25:09 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,4 +1,17 @@
-n = int(input())
-lst = list(map(int, input().split()))
-res = [1] + [lst.index(i) + 2 for i in range(n - 1)]
-print(*res)
+import sys, math
+input = lambda : sys.stdin.readline().rstrip()
+
+dots = {'R' : 0.55, 'G' : 0.7, 'B' : 0.8, 'Y' : 0.85, 'O' : 0.9, 'W' : 0.95}
+for _ in range(int(input())):
+    price, dot, coupon, pay = input().split()
+    price = float(price) * dots[dot]
+    if coupon == 'C':
+        price *= 0.95
+    if pay == 'C':
+        if price - math.floor(price * 10) / 10 < 0.06:
+            price = math.floor(price * 10) / 10
+        else:
+            price = math.ceil(price * 10) / 10
+    else:
+        price = round(price, 2)
+    print(f'${price:.2f}')
