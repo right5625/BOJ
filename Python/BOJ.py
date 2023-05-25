b0235a3888029e438399721cfa9870ae485cd2b4
전진호 <right5625@naver.com>
2023-05-25 11:39:03 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -2,7 +2,14 @@ import sys
 input = lambda : sys.stdin.readline().rstrip()
 
 for c in range(int(input())):
-    B = int(input())
-    S = input().replace('O', '0').replace('I', '1')
-    print(f'Case #{c + 1}: ', end = '')
-    print(*[chr(int(S[i * 8 : (i + 1) * 8], 2)) for i in range(B)], sep = '')
+    S, M, F = input().split()
+    M = int(M[0]) * 12 + int(M[2 : -1])
+    F = int(F[0]) * 12 + int(F[2 : -1])
+    R = M + F + 5 if S == 'B' else M + F - 5
+    if R / 2 == R // 2:
+        start = R // 2 - 4
+        end = start + 8
+    else:
+        start = R // 2 - 3
+        end = start + 7
+    print(f'Case #{c + 1}: {start // 12}\'{start % 12}" to {end // 12}\'{end % 12}"')
