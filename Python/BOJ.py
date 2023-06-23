f7935cb9b83a4ed5e5847ce5da4998a3523e077f
jinho <right5625@naver.com>
2023-06-23 22:04:16 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,15 +1,9 @@
 import sys
 input = lambda : sys.stdin.readline().rstrip()
 
-for c in range(int(input())):
-    S, M, F = input().split()
-    M = int(M[0]) * 12 + int(M[2 : -1])
-    F = int(F[0]) * 12 + int(F[2 : -1])
-    R = M + F + 5 if S == 'B' else M + F - 5
-    if R / 2 == R // 2:
-        start = R // 2 - 4
-        end = start + 8
-    else:
-        start = R // 2 - 3
-        end = start + 7
-    print(f'Case #{c + 1}: {start // 12}\'{start % 12}" to {end // 12}\'{end % 12}"')
+def round_2(val, digits):
+    return round(val + 10 ** (-len(str(val)) - 1), digits)
+
+for _ in range(int(input())):
+    S = list(map(int, input().split()))
+    print(f'{round_2(len(list(filter(lambda x : x > sum(S[1:]) / S[0], S[1:]))) / S[0] * 100, 3):.3f}%')
