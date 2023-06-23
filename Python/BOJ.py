2fc0a8bc9f05aa2a909381fb13814540548e941c
jinho <right5625@naver.com>
2023-06-23 22:07:35 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,9 +1,9 @@
-m = int(input())
-n = int(input())
-res = 0
-for i in range(1, m + 1):
-    for j in range(n, 0, -1):
-        if i + j == 10:
-            res += 1
-            break
-print('There is 1 way to get the sum 10.' if res == 1 else f'There are {res} ways to get the sum 10.')
+import sys
+input = lambda : sys.stdin.readline().rstrip()
+
+def round_2(val, digits):
+    return round(val + 10 ** (-len(str(val)) - 1), digits)
+
+for _ in range(int(input())):
+    S = list(map(int, input().split()))
+    print(f'{round_2(len(list(filter(lambda x : x > sum(S[1:]) / S[0], S[1:]))) / S[0] * 100, 3):.3f}%')
