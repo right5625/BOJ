bb73d14f247834aedfb6f0f0736b9339dd6adeb1
jinho <right5625@naver.com>
2023-06-26 16:39:55 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,9 +1,22 @@
 import sys
 input = lambda : sys.stdin.readline().rstrip()
 
-def round_2(val, digits):
-    return round(val + 10 ** (-len(str(val)) - 1), digits)
-
 for _ in range(int(input())):
-    S = list(map(int, input().split()))
-    print(f'{round_2(len(list(filter(lambda x : x > sum(S[1:]) / S[0], S[1:]))) / S[0] * 100, 3):.3f}%')
+    x1, y1, r1, x2, y2, r2 = map(int, input().split())
+    if x1 == x2 and y1 == y2:
+        print(-1 if r1 == r2 else 0)
+    else:
+        dis = ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
+        if dis < r1 + r2:
+            maxr = max(r1, r2)
+            minr = min(r1, r2)
+            if maxr > minr + dis:
+                print(0)
+            elif maxr == minr + dis:
+                print(1)
+            else:
+                print(2)
+        elif dis == r1 + r2:
+            print(1)
+        else:
+            print(0)
