5622b2171c9e9f5148217a9de4bcaecb2bea240a
jinho <right5625@naver.com>
2023-07-06 02:41:43 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,22 +1,21 @@
-import sys
+import sys, heapq
 input = lambda : sys.stdin.readline().rstrip()
 
-for _ in range(int(input())):
-    x1, y1, r1, x2, y2, r2 = map(int, input().split())
-    if x1 == x2 and y1 == y2:
-        print(-1 if r1 == r2 else 0)
-    else:
-        dis = ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
-        if dis < r1 + r2:
-            maxr = max(r1, r2)
-            minr = min(r1, r2)
-            if maxr > minr + dis:
-                print(0)
-            elif maxr == minr + dis:
-                print(1)
-            else:
-                print(2)
-        elif dis == r1 + r2:
-            print(1)
-        else:
-            print(0)
+V, E = map(int, input().split())
+graph = [[sys.maxsize for __ in range(V + 1)] for _ in range(V + 1)]
+for _ in range(E):
+    s, e, w = map(int, input().split())
+    graph[s][e] = w
+    graph[e][s] = w
+vst = [False] * (V + 1)
+heap = [(0, 1)] # 거리, 정점
+res = 0
+while heap:
+    w, s = heapq.heappop(heap)
+    if not vst[s]:
+        vst[s] = True
+        res += w
+        for e in range(1, V + 1):
+            if graph[s][e] != 0 and not vst[e]: # s 정점과 연결되어 있는 모든 정점 중 아직 방문하지 않은 정점을 선택
+                heapq.heappush(heap, (graph[s][e], e)) # 선택된 정점의 가중치를 우선순위로 지정하여 힙에 삽입
+print(res)
