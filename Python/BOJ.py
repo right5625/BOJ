76932e16e09efdffa8885e59b72577ab91c5c7e2
Jinho <right5625@naver.com>
2023-10-24 14:50:13 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,13 +1,2 @@
-N, K = map(int, input().split())
-h = m = s = res = 0
-while h < N + 1:
-    if K in [h // 10, h % 10, m // 10, m % 10, s // 10, s % 10]:
-        res += 1
-    s += 1
-    if s == 60:
-        s = 0
-        m += 1
-        if m == 60:
-            m = 0
-            h += 1
-print(res)
+x1, x2, n = map(int, input().split())
+if (n == 0 and x1 == x2) or ((x1 - (n - 1)) - (x2 + (n - 1))) > 0 and ((x1 - (n - 1)) - (x2 + (n - 1))) % 2 == 0
