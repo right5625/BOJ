781e0476b4c6678ee44e2c3665b419383bf5e16b
Jinho <right5625@naver.com>
2023-10-24 19:17:01 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,3 +1,2 @@
 w, h = map(int, input().split())
 n, a, b = map(int, input().split())
-print(-1) if w < a or h < b else print(n // ((w // a) * (h // b)) if n // ((w // a) * (h // b)) == n / ((w // a) * (h // b)) else n // ((w // a) * (h // b)) + 1)
