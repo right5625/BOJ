e63af9c94f5d2755620e586b265aac1db03c8b1a
Jinho <right5625@naver.com>
2023-10-24 19:21:12 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,2 +1,2 @@
-w, h = map(int, input().split())
-n, a, b = map(int, input().split())
+x1, x2, n = map(int, input().split())
+print('YES' if x1 == x2 else 'NO') if n == 0 else print('YES' if x1 > x2 and (x1 - x2) % 2 == 0 and (x1 - x2) // 2 >= n else 'NO')
