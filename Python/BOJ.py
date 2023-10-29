74e6a6d5e9ffc6e7d5a63be0b06e09364b05fe25
Jinho <right5625@naver.com>
2023-10-30 00:59:51 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,2 +1,6 @@
-t, r, v = map(float, input().split())
-print(max((t * v - 2 * r) / t, 0))
+for _ in range(15):
+    p = list(input().split())
+    for i in ['w', 'b', 'g']:
+        if i in p:
+            res = {'w' : 'chunbae', 'b' : 'nabi', 'g' : 'yeongcheol'}[i]
+print(res)
