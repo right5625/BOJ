2b8a08184e0014aaf57e8e0df3abe40736bc06c9
Jinho <right5625@naver.com>
2023-10-30 21:54:41 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,6 +1,5 @@
-for _ in range(15):
-    p = list(input().split())
-    for i in ['w', 'b', 'g']:
-        if i in p:
-            res = {'w' : 'chunbae', 'b' : 'nabi', 'g' : 'yeongcheol'}[i]
-print(res)
+lst = []
+for _ in range(6):
+    N, M = map(int, input().split())
+    lst.append(N + M * 10)
+print(max(max(lst[1:]) - lst[0] + 1, 0))
