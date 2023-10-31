97a6d9bf8be798fe26d6f0de44de24faec1e9d96
Jinho <right5625@naver.com>
2023-10-31 17:59:29 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,5 +1,10 @@
-lst = []
-for _ in range(6):
-    N, M = map(int, input().split())
-    lst.append(N + M * 10)
-print(max(max(lst[1:]) - lst[0] + 1, 0))
+import sys
+input = lambda : sys.stdin.readline().rstrip()
+
+dic = {'M' : [0, 0], 'J' : [0, 0]}
+for _ in range(int(input())):
+    MJ, P = input().split()
+    dic[MJ][0] += int(P)
+    dic[MJ][1] += 1
+avg1, avg2 = 0 if dic['M'][0] == 0 else dic['M'][0] / dic['M'][1], 0 if dic['J'][0] == 0 else dic['J'][0] / dic['J'][1]
+print('V') if avg1 == avg2 else print('M' if avg1 > avg2 else 'J')
