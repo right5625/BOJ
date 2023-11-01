19b54cf50850f354bedf2d4e41c27adc65aec58a
Jinho <right5625@naver.com>
2023-11-02 01:40:39 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,10 +1,3 @@
-import sys
-input = lambda : sys.stdin.readline().rstrip()
-
-dic = {'M' : [0, 0], 'J' : [0, 0]}
 for _ in range(int(input())):
-    MJ, P = input().split()
-    dic[MJ][0] += int(P)
-    dic[MJ][1] += 1
-avg1, avg2 = 0 if dic['M'][0] == 0 else dic['M'][0] / dic['M'][1], 0 if dic['J'][0] == 0 else dic['J'][0] / dic['J'][1]
-print('V') if avg1 == avg2 else print('M' if avg1 > avg2 else 'J')
+    s = input()
+    print(min(len(s) - s.count('a'), len(s) - s.count('b')))
