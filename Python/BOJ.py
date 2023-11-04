d48a4ff47f7c84d561026c8e97167af5bbb1ffdf
Jinho <right5625@naver.com>
2023-11-05 00:54:07 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,14 +1,2 @@
-from collections import deque
-
-s = input()
-q = deque()
-q.append((s, 0))
-res = 0
-while q:
-    t, n = q.popleft()
-    if t[0] == t[-1]:
-        res = n
-        break
-    q.append((s[1:], n + 1))
-    q.append((s[:-1], n + 1))
-print('Win' if res % 2 == 1 else 'Lose')
+S, D, I, L, N = map(int, input().split())
+print(max(N * 4 - (S + D + I + L), 0))
