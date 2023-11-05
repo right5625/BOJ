7a0872c7fe4f9fbed89a3e051a265843c1b95951
Jinho <right5625@naver.com>
2023-11-05 10:47:55 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,2 +1,8 @@
-S, D, I, L, N = map(int, input().split())
-print(max(N * 4 - (S + D + I + L), 0))
+n = int(input())
+a = input()
+b = input()
+res = 0
+for i, j in zip(a, b):
+    d = abs(ord(i) - ord(j))
+    res += 26 - d if d > 12 else d
+print(res)
