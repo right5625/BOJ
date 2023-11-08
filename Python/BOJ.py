cb9a611fa8841ef8ec70c22da554ca350a29472e
Jinho <right5625@naver.com>
2023-11-08 13:35:52 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,13 +1,13 @@
-import sys
-input = lambda : sys.stdin.readline().rstrip()
-
-for i in range(int(input())):
-    n = int(input())
-    s = set(input())
-    res = 0
-    for _ in range(n):
-        for j in set(input()):
-            if j in s:
-                res += 1
-                break
-    print(f'Data Set {i + 1}:\n{res}\n')
+n = int(input())
+dic = {str(i) + j : 0 for i in range(1, 6) for j in ['A', 'B', 'C']}
+for i in list(input().split()):
+    dic[i] += 1
+flag = True
+for i in range(1, 5):
+    for j in ['A', 'B', 'C']:
+        if not dic[str(i) + j]:
+            flag = False
+for i in ['5A', '5B', '5C']:
+    if dic[i] < 2:
+        flag = False
+print('TAK' if flag else 'NIE')
