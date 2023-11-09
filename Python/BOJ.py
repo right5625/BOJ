3de49168a0ecf720204c7789a4d2b0fc8a302cd7
Jinho <right5625@naver.com>
2023-11-09 22:46:07 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,13 +1,11 @@
 n = int(input())
-dic = {str(i) + j : 0 for i in range(1, 6) for j in ['A', 'B', 'C']}
-for i in list(input().split()):
-    dic[i] += 1
-flag = True
-for i in range(1, 5):
-    for j in ['A', 'B', 'C']:
-        if not dic[str(i) + j]:
-            flag = False
-for i in ['5A', '5B', '5C']:
-    if dic[i] < 2:
-        flag = False
-print('TAK' if flag else 'NIE')
+d1 = list(map(int, input().split()))
+d2 = list(map(int, input().split()))
+f = s = 0
+for i in d1:
+    for j in d2:
+        if i > j:
+            f += 1
+        elif i < j:
+            s += 1
+print('tie') if f == s else print('first' if f > s else 'second')
