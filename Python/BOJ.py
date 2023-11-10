ab8e416cae64ef3d36033d82ca6c5778d3d6a38c
Jinho <right5625@naver.com>
2023-11-10 17:11:37 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,11 +1,5 @@
-n = int(input())
-d1 = list(map(int, input().split()))
-d2 = list(map(int, input().split()))
-f = s = 0
-for i in d1:
-    for j in d2:
-        if i > j:
-            f += 1
-        elif i < j:
-            s += 1
-print('tie') if f == s else print('first' if f > s else 'second')
+while True:
+    n = int(input())
+    if n == 0:
+        break
+    print('PREMIADO' if n % 42 == 0 else 'TENTE NOVAMENTE')
