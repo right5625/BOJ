1f2ed01ae121a167d44a3610a80fb699edd12a5a
Jinho <right5625@naver.com>
2023-11-13 00:14:21 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,5 +1,5 @@
-while True:
-    n = int(input())
-    if n == 0:
-        break
-    print(*[int(input()) for _ in range(n)][::-1] + [0], sep = '\n')
+for _ in range(int(input())):
+    s = input()
+    if 'S' in s:
+        res = s
+print(res)
