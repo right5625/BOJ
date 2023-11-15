535316f12b996588fe28ad99e5178f228cb60098
Jinho <right5625@naver.com>
2023-11-15 13:27:02 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,2 +1,15 @@
-a = list(int(input()) for _ in range(4))
-print(a[0] * a[2])
+import sys
+input = lambda : sys.stdin.readline().rstrip()
+
+while True:
+    n, m = map(int, input().split())
+    if n == m == 0:
+        break
+    lst = [False] * (n + 1)
+    lst[m] = True
+    for _ in range(n - 2):
+        lst[int(input())] = True
+    for i in range(1, n + 1):
+        if not lst[i]:
+            print(i)
+            break
