d97243bda6a78fa0b79c66fb01a01bcf6556690d
Jinho <right5625@naver.com>
2023-11-16 11:54:23 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,15 +1,12 @@
-import sys
-input = lambda : sys.stdin.readline().rstrip()
-
-while True:
-    n, m = map(int, input().split())
-    if n == m == 0:
-        break
-    lst = [False] * (n + 1)
-    lst[m] = True
-    for _ in range(n - 2):
-        lst[int(input())] = True
-    for i in range(1, n + 1):
-        if not lst[i]:
-            print(i)
-            break
+N, M = map(int, input().split())
+a = list(map(int, input().split()))
+res = []
+group = cur = 0
+for i in a:
+    if cur + i <= M:
+        cur += i
+    else:
+        group += 1
+        cur = i
+    res.append(group)
+print(*res, sep = '\n')
