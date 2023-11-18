b757e4f2e384b1925ccbdb77b3d033908d7f419a
Jinho <right5625@naver.com>
2023-11-18 11:55:11 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,13 +1,4 @@
-import heapq
-
-a1, a2, a3, b1, b2, b3, L = map(int, input().split())
-heap = [-a1] * b1 + [-a2] * b2 + [-a3] * b3
-heapq.heapify(heap)
-l = res = 0
-while l < L:
-    if not heap:
-        res = 0
-        break
-    l += -heapq.heappop(heap)
-    res += 1
-print(res)
+N = int(input())
+M = input()
+K = int(input())
+print(K - 1 if K % 2 == 0 else K) if M == 'annyong' else print(K - 1 if K != 1 else K + 1) if K % 2 == 1 else print(K)
