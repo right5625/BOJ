da280c39de3df9d28969734f8ab3f7813a788d4c
Jinho <right5625@naver.com>
2023-11-22 13:20:41 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,8 +1,11 @@
-for i in range(int(input())):
-    N = int(input())
-    H = list(map(int, input().split()))
-    res = 0
-    for j in range(1, N - 1):
-        if H[j] > H[j - 1] and H[j] > H[j + 1]:
-            res +=1
-    print(f'Case #{i + 1}: {res}')
+N = int(input())
+A = [input() for _ in range(N)]
+for i in range(1, N):
+    if A[i] == 'Present!':
+        A[i - 1] = A[i] = False
+if len(A) == A.count(False):
+    print('No Absences')
+else:
+    for i in A:
+        if i:
+            print(i)
