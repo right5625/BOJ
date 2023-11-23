61b83c61ce8ebd4c5600a6e82c709699e29bbc2c
Jinho <right5625@naver.com>
2023-11-23 12:17:05 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,11 +1,5 @@
-N = int(input())
-A = [input() for _ in range(N)]
-for i in range(1, N):
-    if A[i] == 'Present!':
-        A[i - 1] = A[i] = False
-if len(A) == A.count(False):
-    print('No Absences')
-else:
-    for i in A:
-        if i:
-            print(i)
+lst = []
+for _ in range(int(input())):
+    x, y = map(int, input().split())
+    lst.append(y)
+print(abs(max(lst) - min(lst)))
