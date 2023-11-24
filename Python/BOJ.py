22e22d50ccf2a0405081157c49b530bafad00f09
Jinho <right5625@naver.com>
2023-11-24 21:28:21 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,5 +1,7 @@
-lst = []
-for _ in range(int(input())):
-    x, y = map(int, input().split())
-    lst.append(y)
-print(abs(max(lst) - min(lst)))
+from math import ceil
+
+h1, d1, t1 = map(int, input().split())
+h2, d2, t2 = map(int, input().split())
+r1 = ceil(max((h2 - d1), 0) / d1) * t1
+r2 = ceil(max((h1 - d2), 0) / d2) * t2
+print('draw') if r1 == r2 else print('player one' if r1 < r2 else 'player two')
