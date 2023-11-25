484880a633de9a3a94fc86f779eef0eee079ffe7
Jinho <right5625@naver.com>
2023-11-25 22:07:24 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,7 +1,14 @@
-from math import ceil
+import sys
+input = lambda : sys.stdin.readline().rstrip()
 
-h1, d1, t1 = map(int, input().split())
-h2, d2, t2 = map(int, input().split())
-r1 = ceil(max((h2 - d1), 0) / d1) * t1
-r2 = ceil(max((h1 - d2), 0) / d2) * t2
-print('draw') if r1 == r2 else print('player one' if r1 < r2 else 'player two')
+res = prevl = prevr = 0
+for _ in range(int(input())):
+    l, r = map(int, input().split())
+    if l and r and l == r:
+        res += 1
+    if l and l == prevl:
+        res += 1
+    if r and r == prevr:
+        res += 1
+    prevl, prevr = l, r
+print(res)
