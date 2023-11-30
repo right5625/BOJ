f66240c73e3d5ec3cb8520b00904b485fff39739
Jinho <right5625@naver.com>
2023-11-30 23:37:29 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,8 +1,2 @@
-N = int(input())
-S = list(map(int, input().split()))
-T, P = map(int, input().split())
-res = 0
-for i in S:
-    res += i // T if i % T == 0 else i // T + 1
-s = sum(S)
-print(f'{res}\n{s // P} {s % P}')
+from math import pi
+print((((int(input()) / pi) ** 0.5 + 1) * 2) ** 2)
