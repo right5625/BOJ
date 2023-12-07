2c5c3f2ce6a002379c0d37265487c3de8d7b0f4e
Jinho <right5625@naver.com>
2023-12-07 13:37:30 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,3 +1,10 @@
-M, K = map(int, input().split())
-S = sum(list(map(int, input().split())))
-print(S // M if S % M == 0 else S // M + 1)
+N = int(input())
+DP = [[0 for _ in range(N + 1)] for _ in range(N + 1)]
+for i in range(1, N + 1):
+    DP[i][0] = 1
+    DP[i][i] = 1
+    DP[i][1] = i
+for i in range(3, N + 1):
+    for j in range(2, i):
+        DP[i][j] = DP[i - 1][j - 1] + DP[i - 1][j]
+print(DP[N][5])
