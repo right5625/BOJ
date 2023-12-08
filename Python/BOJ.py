41d4c6360453fbb3be9c3f4a4547205964b3aa93
Jinho <right5625@naver.com>
2023-12-09 00:38:04 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,10 +1,3 @@
-N = int(input())
-DP = [[0 for _ in range(N + 1)] for _ in range(N + 1)]
-for i in range(1, N + 1):
-    DP[i][0] = 1
-    DP[i][i] = 1
-    DP[i][1] = i
-for i in range(3, N + 1):
-    for j in range(2, i):
-        DP[i][j] = DP[i - 1][j - 1] + DP[i - 1][j]
-print(DP[N][5])
+n = int(input())
+S = input()
+print(min(S.count('u'), S.count('o'), S.count('s'), S.count('p'), S.count('c')))
