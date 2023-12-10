046a5de95c20a4c1ecba6345760845fb1fc8b859
Jinho <right5625@naver.com>
2023-12-10 21:38:38 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,3 +1,16 @@
+def star(n, p, c):
+    if n == 3:
+        res[p].append(c * 3)
+        res[p + 1].append(c + ' ' + c)
+        res[p + 2].append(c * 3)
+        return
+    for i in range(3):
+        star(n // 3, n // 3 * i + p, c)
+        star(n // 3, n // 3 * i + p, ' ') if i % 2 == 1 else star(n // 3, n // 3 * i + p, c)    
+        star(n // 3, n // 3 * i + p, c)
+
 N = int(input())
-A = list(map(int, input().split()))
-print(min(A[:A.index(-1)]) + min(A[A.index(-1) + 1:]))
+res = [[] for _ in range(N)]
+star(N, 0, '*')
+for i in res:
+    print(''.join(i))
