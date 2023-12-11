b3a350db4c292d01191660caf8265ca5f47a4bef
Jinho <right5625@naver.com>
2023-12-11 23:48:00 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,6 +1,4 @@
-seat = {chr(i) : ['.'] * 20 for i in range(ord('A'), ord('J') + 1)}
-for _ in range(int(input())):
-    S = input()
-    seat[S[0]][int(S[1:]) - 1] = 'o'
-for i in seat.values():
-    print(*i, sep = '')
+from collections import Counter
+
+N, M = map(int, input().split())
+print(max(Counter(list(map(int, input().split()))).values()))
