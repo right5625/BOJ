0bf04206cd8fbde73681e994780bb1c4c431af01
Jinho <right5625@naver.com>
2023-12-12 13:54:37 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,4 +1,11 @@
-from collections import Counter
-
-N, M = map(int, input().split())
-print(max(Counter(list(map(int, input().split()))).values()))
+N, K = map(int, input().split())
+P = [input().split() for _ in range(N)]
+res = []
+for i in P:
+    t = ''
+    for j in i:
+        t += j * K
+    for _ in range(K):
+        res.append(t)
+for i in res:
+    print(*i)
