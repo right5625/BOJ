ad8e84ea5e56d2ea2af5333e26291e9a068b60c0
Jinho <right5625@naver.com>
2023-12-15 14:43:22 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,3 +1,11 @@
-T = input()
-key = ord('C') ^ ord(T[0])
-print(*[chr(ord(i) ^ key) for i in T], sep = '')
+res = 0
+input()
+while True:
+    S = input()
+    if S == '고무오리 디버깅 끝':
+        print('힝구' if res else '고무오리야 사랑해')
+        break
+    elif S == '문제':
+        res += 1
+    else:
+        res += -1 if res else 2
