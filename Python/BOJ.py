b3d3a89fa52c17c1f19a75452afd62ce155b3fed
Jinho <right5625@naver.com>
2023-12-16 13:19:41 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,11 +1,1 @@
-res = 0
-input()
-while True:
-    S = input()
-    if S == '고무오리 디버깅 끝':
-        print('힝구' if res else '고무오리야 사랑해')
-        break
-    elif S == '문제':
-        res += 1
-    else:
-        res += -1 if res else 2
+print(sum([len(input()) for _ in range(int(input()))]))
