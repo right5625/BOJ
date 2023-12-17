0009b3015c1efd81726fdda02b2a3ad8190ea1fb
Jinho <right5625@naver.com>
2023-12-18 00:30:32 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,1 +1,10 @@
-print(sum([len(input()) for _ in range(int(input()))]))
+dic = {'B' : 0, 'S' : 0, 'A' : 0}
+N = int(input())
+for i in input():
+    dic[i] += 1
+if dic['B'] == dic['S'] == dic['A']:
+    print('SCU')
+else:
+    for k, v in dic.items():
+        if v == max(dic.values()):
+            print(k, end = '')
