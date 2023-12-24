54c5adda7c31c34ee91f0cb2c4afae9376a65470
Jinho <right5625@naver.com>
2023-12-24 20:46:05 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,9 +1,6 @@
-import sys
-input = lambda : sys.stdin.readline().rstrip()
-
-res = []
-for _ in range(int(input())):
-    name = input()
-    if len(name) == 3:
-        res.append(name)
-print(sorted(res)[0])
+N, M = map(int, input().split())
+res = 0
+for _ in range(N):
+    if input().count('O') > M // 2:
+        res += 1
+print(res)
