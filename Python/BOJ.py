3a3b6d4191d273769cfe57aae0293c9f2decdeae
Jinho <right5625@naver.com>
2023-12-27 11:43:29 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,8 +1,15 @@
-import sys
-input = lambda : sys.stdin.readline().rstrip()
-
-card = {i : 0 for i in ['STRAWBERRY', 'BANANA', 'LIME', 'PLUM']}
-for _ in range(int(input())):
-    S, X = input().split()
-    card[S] += int(X)
-print('YES' if 5 in list(card.values()) else 'NO')
+input()
+dic = {i : 0 for i in 'roygbivROYGBIV'}
+for i in input():
+    if i in 'roygbivROYGBIV':
+        dic[i] = 1
+res1 = sum(list(dic[i] for i in 'roygbiv'))
+res2 = sum(list(dic[i] for i in 'ROYGBIV'))
+if res1 == 7 and res2 == 7:
+    print('YeS')
+elif res1 == 7:
+    print('yes')
+elif res2 == 7:
+    print('YES')
+else:
+    print('NO!')
