337c7c8bfb92732a9f885f351be76d534bc38a29
Jinho <right5625@naver.com>
2023-12-30 22:17:58 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,16 +1,15 @@
-import string
-
-J, N = map(int, input().split())
-res = 0
-for _ in range(N):
-    j = 0
-    for i in input():
-        if i in string.ascii_uppercase:
-            j += 4
-        elif i == ' ':
-            j += 1
+N = int(input())
+a = []
+while len(a) < N:
+    a += list(map(int, input().split()))
+res = temp = 0
+for i in sorted(a)[::-1]:
+    if i % 2 == 0:
+        res += i
+    else:
+        if temp % 2 == 1:
+            res += (i + temp)
+            temp = 0
         else:
-            j += 2
-    if j <= J:
-        res += 1
-print(res)
+            temp = i
+print(res // 2)
