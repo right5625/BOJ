4ad1031658d1e79535362beffd3f6c5cf22641c4
Jinho <right5625@naver.com>
2024-01-02 20:32:12 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,3 +1,16 @@
-for _ in range(int(input())):
-    N = input()
-    print('Good' if (int(N) + 1) % int(N[2:]) == 0 else 'Bye')
+N = int(input())
+P = list(map(int, input().split()))
+res = cur = 0
+strick = True
+for i in P:
+    if i:
+        cur += 1
+    else:
+        if strick:
+            strick = False
+            continue
+        else:
+            res = max(res, cur)
+            cur = 0
+    strick = True
+print(max(res, cur))
