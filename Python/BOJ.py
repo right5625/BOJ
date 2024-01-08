853662107ddb506841d37faaeed4e646a5b2137a
Jinho <right5625@naver.com>
2024-01-08 15:56:16 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,7 +1,8 @@
-N, M = map(int, input().split())
-cube = [list(map(int, input().split())) for _ in range(M)]
-res = 0
-for i in cube:
-    if cube.count([i[0] - 1, i[1], i[2]]) and cube.count([i[0] + 1, i[1], i[2]]) and cube.count([i[0], i[1] - 1, i[2]]) and cube.count([i[0], i[1] + 1, i[2]]) and cube.count([i[0], i[1], i[2] - 1]) and cube.count([i[0], i[1], i[2] + 1]):
-        res += 1
+N = int(input())
+s = input()
+res = 1
+for i in 'yuiophjklbnm':
+    if i == s[-1]:
+        res = 0
+        break
 print(res)
