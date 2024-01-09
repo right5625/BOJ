89c946223aaa38c26c5682408a50885eb173cc3c
Jinho <right5625@naver.com>
2024-01-09 14:05:42 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,8 +1,18 @@
-N = int(input())
-s = input()
-res = 1
-for i in 'yuiophjklbnm':
-    if i == s[-1]:
-        res = 0
-        break
-print(res)
+W, N = input().split()
+C = [list(map(int, input().split())) for _ in range(int(N))]
+if W in 'LR':
+    for i in range(int(N)):
+        C[i] = C[i][::-1]
+else:
+    C = C[::-1]
+for i in C:
+    for j in i:
+        if j in [3, 4, 6, 7, 9]:
+            print('?', end = ' ')
+        elif j == 2:
+            print(5, end = ' ')
+        elif j == 5:
+            print(2, end = ' ')
+        else:
+            print(j, end = ' ')
+    print()
