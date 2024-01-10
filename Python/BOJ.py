e8feb05c7fafbd7d0ee15fc6bb273e15a707466d
Jinho <right5625@naver.com>
2024-01-10 15:22:20 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,18 +1,5 @@
-W, N = input().split()
-C = [list(map(int, input().split())) for _ in range(int(N))]
-if W in 'LR':
-    for i in range(int(N)):
-        C[i] = C[i][::-1]
-else:
-    C = C[::-1]
-for i in C:
-    for j in i:
-        if j in [3, 4, 6, 7, 9]:
-            print('?', end = ' ')
-        elif j == 2:
-            print(5, end = ' ')
-        elif j == 5:
-            print(2, end = ' ')
-        else:
-            print(j, end = ' ')
-    print()
+p = sorted([float(input()) for _ in range(10)])[::-1]
+res = 1
+for i in range(9):
+    res *= (p[i] / (i + 1))
+print(res * 10 ** 9)
