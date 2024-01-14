78ccee1ab6ab8db96eb2554bf658bbab0d641c9d
Jinho <right5625@naver.com>
2024-01-14 15:25:10 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,7 +1,13 @@
-A = list(input().split())
-B = input()
-res = 0
-for i in A:
-    if i != B and i[:len(B)] == B:
-        res += 1
-print(res)
+import sys
+input = lambda : sys.stdin.readline().rstrip()
+
+N, K = map(int, input().split())
+lst = [0]
+cnt = 0
+for i in [list(map(int, input().split())) for _ in range(N)]:
+    if i[0] < i[1]:
+        lst.append(i[1] - i[0])
+    else:
+        cnt += 1
+lst.sort()
+print(lst[max(K - cnt, 0)])
