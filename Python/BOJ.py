ad91145f9d17e6c008b95cca566724022a6d1b6b
Jinho <right5625@naver.com>
2024-01-18 15:16:48 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,15 +1,9 @@
-white = []
-black = []
-n = 0
-for i in range(8):
-    for j in range(1, 9):
-        if j % 2 == n:
-            white.append('ABCDEFGH'[i] + str(j))
-            white.append(str(i * 8 + j))
-        else:
-            black.append('ABCDEFGH'[i] + str(j))
-            black.append(str(i * 8 + j))
-    n = abs(n - 1)
-for _ in range(int(input())):
-    n1, n2 = input().split()
-    print('YES' if (n1 in white and n2 in white) or (n1 in black and n2 in black) else 'NO')
+import sys
+input = lambda : sys.stdin.readline().rstrip()
+
+res = m = 0
+for i in [int(input()) for _ in range(int(input()))][::-1]:
+    if m < i:
+        m = i
+        res += 1
+print(res)
