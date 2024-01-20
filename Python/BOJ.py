8f449a1f2e3bf4af72d43d212602a43102fe00e2
Jinho <right5625@naver.com>
2024-01-20 15:27:17 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,4 +1,2 @@
-N = int(input())
-for i in range(N, 1, -1):
-    print(f'{i} bottles of beer on the wall, {i} bottles of beer.\nTake one down and pass it around, {i - 1} ' + ('bottle' if i - 1 == 1 else 'bottles') + ' of beer on the wall.\n')
-print(f'1 bottle of beer on the wall, 1 bottle of beer.\nTake one down and pass it around, no more bottles of beer on the wall.\n\nNo more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, {N} ' + ('bottle' if N == 1 else 'bottles') + ' of beer on the wall.')
+a, b, c = map(int, input().split())
+print('YES' if c >= abs(a) + abs(b) and (c - (abs(a) + abs(b))) % 2 == 0 else 'NO')
