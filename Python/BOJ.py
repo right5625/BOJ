a213e35583d1de9e1848e55d8bbf45347c757197
Jinho <right5625@naver.com>
2024-01-23 01:48:02 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,11 +1,1 @@
-N = int(input())
-res = 1
-prev = temp = 0
-for i in list(map(int, input().split())):
-    if prev <= i:
-        temp += 1
-    else:
-        res = max(res, temp)
-        temp = 1
-    prev = i
-print(max(res, temp))
+print(sorted(list(map(int, input().split())))[1])
