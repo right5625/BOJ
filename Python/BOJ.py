331ca45f0c10a6a575e84e926a11d419324d614b
Jinho <right5625@naver.com>
2024-01-26 22:32:06 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,6 +1,6 @@
-import sys
-input = lambda : sys.stdin.readline().rstrip()
-
-for _ in range(int(input())):
-    a, b = map(int, input().split())
-    print((a + b - 1) * (a + b) // 2 * (a + b))
+m1 = [tuple(map(int, input().split())) for _ in range(4)]
+m2 = list(zip(*m1))
+cnt = set()
+for i in m1 + m2:
+    cnt.add(sum(i))
+print('magic' if len(cnt) == 1 else 'not magic')
