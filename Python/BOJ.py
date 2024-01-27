bfdda2812754c2fa3f59b413005d408987bb24ca
Jinho <right5625@naver.com>
2024-01-27 13:23:07 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,6 +1,6 @@
-m1 = [tuple(map(int, input().split())) for _ in range(4)]
-m2 = list(zip(*m1))
-cnt = set()
-for i in m1 + m2:
-    cnt.add(sum(i))
-print('magic' if len(cnt) == 1 else 'not magic')
+A = int(input())
+B = int(input())
+C = int(input())
+S = str(A * B * C)
+for i in range(10):
+    print(S.count(str(i)))
