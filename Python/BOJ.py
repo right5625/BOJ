b00e106f64a6a4c07abd12a4a17bce0660447560
Jinho <right5625@naver.com>
2024-01-30 23:09:57 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,2 +1,4 @@
-for i in input():
-    print(i * sum(list(map(int, str(ord(i))))))
+E = [i + 1 for i in range(int(input()))]
+while len(E) != 1:
+    E = [E[i] for i in range(len(E)) if i % 2 == 1]
+print(E[0])
