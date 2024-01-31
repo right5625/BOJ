2d630f2f41a55bf376ada520556e795ce3cb4f5d
Jinho <right5625@naver.com>
2024-01-31 11:46:47 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,4 +1,16 @@
-E = [i + 1 for i in range(int(input()))]
-while len(E) != 1:
-    E = [E[i] for i in range(len(E)) if i % 2 == 1]
-print(E[0])
+N, M = map(int, input().split())
+L = [0] + list(map(int, input().split()))
+for _ in range(M):
+    a, b, c = map(int, input().split())
+    if a == 1:
+        L[b] = c
+    elif a == 2:
+        for i in range(b, c + 1):
+            L[i] = abs(L[i] - 1)
+    elif a == 3:
+        for i in range(b, c + 1):
+            L[i] = 0
+    else:
+        for i in range(b, c + 1):
+            L[i] = 1
+print(*L[1:])
