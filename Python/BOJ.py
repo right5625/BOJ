c0373439a0bfb0c9aa82362527217d6542a93349
Jinho <right5625@naver.com>
2024-02-01 16:12:37 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,16 +1,7 @@
-N, M = map(int, input().split())
-L = [0] + list(map(int, input().split()))
-for _ in range(M):
-    a, b, c = map(int, input().split())
-    if a == 1:
-        L[b] = c
-    elif a == 2:
-        for i in range(b, c + 1):
-            L[i] = abs(L[i] - 1)
-    elif a == 3:
-        for i in range(b, c + 1):
-            L[i] = 0
-    else:
-        for i in range(b, c + 1):
-            L[i] = 1
-print(*L[1:])
+import heapq
+
+heap = []
+for _ in range(int(input())):
+    s, n = input().split()
+    heapq.heappush(heap, [int(n), s])
+print(heapq.heappop(heap)[1])
