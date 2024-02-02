f5a0427563bf97d13c3e66c32368987ccf106167
Jinho <right5625@naver.com>
2024-02-02 17:03:40 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,7 +1,9 @@
-import heapq
-
-heap = []
-for _ in range(int(input())):
-    s, n = input().split()
-    heapq.heappush(heap, [int(n), s])
-print(heapq.heappop(heap)[1])
+m = [int(input()) for _ in range(int(input()))]
+if 3 in m:
+    print('None')
+elif m.count(5) == len(m):
+    print('Named')
+elif sum(m) / len(m) >= 4.5:
+    print('High')
+else:
+    print('Common')
