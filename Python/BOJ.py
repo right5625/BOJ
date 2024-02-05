fbda153677c61401639e2d8769e0413a0bb206cd
Jinho <right5625@naver.com>
2024-02-05 14:20:57 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,11 +1,4 @@
-n, a, b = map(int, input().split())
-w = [int(input()) for _ in range(n - 1)]
-if a not in w and b not in w:
-    print(-1)
-elif a not in w:
-    print(a)
-elif b not in w:
-    print(b)
-else:
-    for i in range(a, b + 1):
-        print(i)
+A = int(input())
+B = int(input())
+C = int(input())
+print(f'{A + B - C}\n{int(str(A) + str(B)) - C}')
