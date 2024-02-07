8e5f0239da093ff04ef64132458891cb7a8a2620
Jinho <right5625@naver.com>
2024-02-07 13:36:06 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,9 +1,5 @@
-S = input()
-if 'A' in S:
-    print(S.replace('B', 'A').replace('C', 'A').replace('D', 'A').replace('F', 'A'))
-elif 'B' in S:
-    print(S.replace('C', 'B').replace('D', 'B').replace('F', 'B'))
-elif 'C' in S:
-    print(S.replace('D', 'C').replace('F', 'C'))
-else:
-    print('A' * len(S))
+import sys
+input = lambda : sys.stdin.readline().rstrip()
+
+for _ in range(int(input())):
+    print(int(input()) * 23)
