66fb996d892a3aa86e70f034c7b417f0b9bb8303
Jinho <right5625@naver.com>
2024-02-16 23:55:18 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,1 +1,2 @@
-print('cute' if input()[-5:] == 'driip' else 'not cute')
+S = [input() for _ in range(int(input()))]
+print(''.join([S[0][i] if len(set([j[i] for j in S])) == 1 else '?' for i in range(len(S[0]))]))
