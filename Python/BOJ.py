7f97c43a846d09e700e7a04dfac5661364a7a5f0
Jinho <right5625@naver.com>
2024-02-22 12:36:18 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,1 +1,11 @@
-print(int(str(sum(list(map(int, input()[::-1].split()))))[::-1]))
+lst = [[4, 7]]
+while len(str(lst[-1][0])) < 6:
+    lst.append([int(i + str(j)) for i in '47' for j in lst[-1]])
+N = int(input())
+lst = sum(lst, [])
+res = 0
+for i in range(len(lst)):
+    if lst[i] > N:
+        res = lst[i - 1]
+        break
+print(res if res != 0 else lst[-1])
