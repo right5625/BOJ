fea03812456593b28607280ed37b2f171d46f857
Jinho <right5625@naver.com>
2024-02-24 11:28:02 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,1 +1,5 @@
-print(oct(int(input(), 2))[2:])
+N, K = map(int, input().split())
+A = list(map(int, input().split(',')))
+for i in range(K):
+    A = [A[j + 1] - A[j] for j in range(N - (i + 1))]
+print(*A, sep = ',')
