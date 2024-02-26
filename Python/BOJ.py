253f49f516e03c34325fddb13b3948152175a7cc
Jinho <right5625@naver.com>
2024-02-26 23:37:14 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,7 +1,4 @@
-N = int(input())
-n, d = 10, -1
-while N > n:
-    N = round(N + 0.00000001, d)
-    n *= 10
-    d -= 1
-print(int(N))
+K = int(input())
+S = input()
+for i in list(zip(*[S[K * i : K * (i + 1)] if i % 2 == 0 else S[K * i : K * (i + 1)][::-1] for i in range(len(S) // K)])):
+    print(*i, sep = '', end = '')
