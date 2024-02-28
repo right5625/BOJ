ba3e3a8c755fe573666734c97a95e40f57e67459
Jinho <right5625@naver.com>
2024-02-28 15:00:41 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,9 +1,7 @@
-R, C, ZR, ZC = map(int, input().split())
-res = []
-for i in [input() for _ in range(R)]:
-    t = ''
-    for j in range(C):
-        t += i[j] * ZC
-    for _ in range(ZR):
-        res.append(t)
-print(*res, sep = '\n')
+N = int(input())
+res = 0
+i = 1
+while (N * i + i) % N != 0:
+    res += N * i + i
+    i += 1
+print(res)
