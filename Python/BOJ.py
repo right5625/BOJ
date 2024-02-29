6265f5bc5c29e71cd1e75fbcb9728c1acb4da611
Jinho <right5625@naver.com>
2024-02-29 14:11:23 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,7 +1,3 @@
-N = int(input())
-res = 0
-i = 1
-while (N * i + i) % N != 0:
-    res += N * i + i
-    i += 1
-print(res)
+for _ in range(int(input())):
+    A, B = input().split()
+    print(bin(int(A, 2) + int(B, 2))[2:])
