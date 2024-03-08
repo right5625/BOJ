c0bbc567b4f391cf720bf0ecd596145304db98db
Jinho <right5625@naver.com>
2024-03-08 14:27:40 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,1 +1,9 @@
-print(int((int(input()) // 2) ** 0.5) * 8)
+for i in range(1, 10):
+    print('? A', i, flush = True)
+    if int(input()):
+        for j in range(1, 10):
+            print('? B', j, flush = True)
+            if int(input()):
+                break
+        print('!', i + j)
+        break
