7e3d243df9621bbcc1fa0892ad9f3b4cdd18d742
Jinho <right5625@naver.com>
2024-03-13 11:55:13 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,9 +1,1 @@
-n = int(input())
-res = temp = 0
-for i in list(map(int, input().split())):
-    if i:
-        temp += 1
-    else:
-        res = max(res, temp)
-        temp = 0
-print(max(res, temp))
+print(int(input()) + int(input()) + 3)
