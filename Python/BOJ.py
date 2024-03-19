b72c6b64313184080f06a49fa83a7bb5f6001592
right5625 <right5625@naver.com>
2024-03-19 12:29:08 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,8 +1,2 @@
-N = input()
-S = input()
-T = input()
-res = 0
-for i, j in zip(S, T):
-    if i != j:
-        res += 1
-print(res)
+N = int(input())
+print(*sorted(set(map(int, input().split()))), sep = '\n')
