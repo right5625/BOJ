f81b853f3fba33ce7c7263e79573a6b37988650f
right5625 <right5625@naver.com>
2024-03-26 14:02:19 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,6 +1,11 @@
-prev = res = ''
-for i in input():
-    if prev != i:
-        res += i
-    prev = i
+K = int(input())
+N = int(input())
+A = list(map(int, input().split()))
+M = int(input())
+B = list(map(int, input().split()))
+res = 0
+for i in A:
+    for j in B:
+        if j - i == K:
+            res += 1
 print(res)
