c01d8ca2781463d478bb5c6615638c3fa1192306
right5625 <right5625@naver.com>
2024-04-01 20:16:25 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,6 +1,4 @@
-mx, res = 0, ''
-for i in [list(input().split()) for _ in range(int(input()))]:
-    if i[2] == 'Russia' and mx < int(i[0]):
-        res = i[1]
-        mx = int(i[0])
-print(res)
+N = int(input())
+M = int(input())
+K = int(input())
+print(int(M * (K / N)))
