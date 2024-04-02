0917d1f0f436e76d53d011a0a5081830b4afdb5a
right5625 <right5625@naver.com>
2024-04-03 01:01:56 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,4 +1,8 @@
-N = int(input())
-M = int(input())
-K = int(input())
-print(int(M * (K / N)))
+N, M = map(int, input().split())
+lst = list(zip(*[list(input()) for _ in range(N)]))
+res = 'ESCAPE FAILED'
+for i in range(M):
+    if len(set(lst[i])) == 1 and lst[i][0] == 'X':
+        res = i + 1
+        break
+print(res)
