970a262b6b00ca1f0e7fedc4fa2fd056f2e866fd
right5625 <right5625@naver.com>
2024-04-05 23:58:43 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,5 +1,1 @@
-for _ in range(int(input())):
-    n = input()
-    if n[len(n) - 1] == '!':
-        n = n.rstrip('!')[:-1] + '1'
-    print(0 if n.count('!') % 2 == 0 else 1) if n[-1] == '0' else print(1 if n.count('!') % 2 == 0 else 0)
+print('SciComLove' if int(input()) % 2 == 0 else 'evoLmoCicS')
