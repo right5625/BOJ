9e4b7f64346c3d96512e028ac8602e184f317402
right5625 <right5625@naver.com>
2024-04-09 12:16:44 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,10 +1,13 @@
-n = int(input())
-if n < 3:
-    print(1)
-else:
-    prev2, prev1 = 0, 1
-    for i in range(n - 2):
-        temp = prev2 + prev1
-        prev2 = prev1
-        prev1 = temp
-    print(prev2 + prev1)
+from collections import defaultdict
+
+N = int(input())
+A = list(map(int, input().split()))
+D = defaultdict(bool)
+for i in A:
+    D[i] = True
+res = 'No'
+for i in A:
+    if D[i] and D[i + 3] and D[i + 6]:
+        res = 'Yes'
+        break
+print(res)
