ae4fe2bc382410fc0feb7b44e5e10cd4dab8a747
right5625 <right5625@naver.com>
2024-04-12 21:26:09 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,7 +1,12 @@
-l, r = map(int, input().split())
-res = -1
-for i in range(l, r + 1):
-    if len(str(i)) == len(set(str(i))):
-        res = i
-        break
-print(res)
+for c in range(int(input())):
+    N = int(input())
+    AB = list(map(int, input().split()))
+    res = []
+    for i in [int(input()) for _ in range(int(input()))]:
+        cnt = 0
+        for j in range(0, N * 2, 2):
+            if AB[j] <= i <= AB[j + 1]:
+                cnt += 1
+        res.append(cnt)
+    print(f'Case #{c + 1}:', *res)
+    input()
