43b6af7ce40d49361a5ad0f4e38ee30d80f72048
right5625 <right5625@naver.com>
2024-04-13 14:37:31 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,12 +1,5 @@
-for c in range(int(input())):
-    N = int(input())
-    AB = list(map(int, input().split()))
-    res = []
-    for i in [int(input()) for _ in range(int(input()))]:
-        cnt = 0
-        for j in range(0, N * 2, 2):
-            if AB[j] <= i <= AB[j + 1]:
-                cnt += 1
-        res.append(cnt)
-    print(f'Case #{c + 1}:', *res)
-    input()
+lst = [int(input()) for _ in range(int(input()))]
+res = 0
+for i in set(lst):
+    res = max(res, lst.count(i))
+print(res)
