effcf4995ef3b32ec8da4f5ee10565369d0a65b2
right5625 <right5625@naver.com>
2024-04-17 23:38:03 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,2 +1,17 @@
-input()
-print(15000 - len(set(list(map(int, input().split())))))
+AB = {'A' : 0, 'B' : 0}
+while True:
+    S = input().split()
+    if S[0] == '1':
+        AB[S[1]] = int(S[2]) if S[2] not in 'AB' else AB[S[2]]
+    elif S[0] == '2':
+        print(AB[S[1]])
+    elif S[0] == '3':
+        AB[S[1]] += int(S[2]) if S[2] not in 'AB' else AB[S[2]]
+    elif S[0] == '4':
+        AB[S[1]] *= int(S[2]) if S[2] not in 'AB' else AB[S[2]]
+    elif S[0] == '5':
+        AB[S[1]] -= int(S[2]) if S[2] not in 'AB' else AB[S[2]]
+    elif S[0] == '6':
+        AB[S[1]] //= int(S[2]) if S[2] not in 'AB' else AB[S[2]]
+    else:
+        break
