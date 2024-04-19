502dd6000a14a7bbe56a4aa24efda1754bb99342
right5625 <right5625@naver.com>
2024-04-20 00:56:48 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,1 +1,5 @@
-print(max([len(set(input())) for _ in range(int(input()))]))
+for _ in range(int(input())):
+    N, C, S = input().split()
+    for _ in range(int(C)):
+        S = S[int(N):] + S
+    print(S)
