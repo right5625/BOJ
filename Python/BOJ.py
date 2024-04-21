58b290249185f25eb474e4ba8d38a7f7e0d7c86e
right5625 <right5625@naver.com>
2024-04-21 12:52:26 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,5 +1,19 @@
-while True:
-    n = int(input())
-    if n == -1:
-        break
-    print(int('0b' + bin(n)[2:].zfill(32)[::-1], 2))
+A = input()
+B = input()
+C = 'ABCDE'
+P = ''
+for i in range(0, 10, 2):
+    temp = A[i : i + 2] + B[i : i + 2]
+    if temp.count(C[i // 2]) == 0:
+        P += C[i // 2].lower()
+    elif temp.count(C[i // 2]) == 1 or temp.count(C[i // 2] + C[i // 2].lower()) == 2:
+        P += C[i // 2] + C[i // 2].lower()
+    else:
+        P += C[i // 2]
+for _ in range(int(input())):
+    flag = True
+    for i in input():
+        if i not in P:
+            flag = False
+            break
+    print('Possible baby.' if flag else 'Not their baby!')
