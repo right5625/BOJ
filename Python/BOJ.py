ecf5c29ce464fdd65d712a0aae45e8611613ad37
right5625 <right5625@naver.com>
2024-04-22 14:39:28 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,19 +1,10 @@
-A = input()
-B = input()
-C = 'ABCDE'
-P = ''
-for i in range(0, 10, 2):
-    temp = A[i : i + 2] + B[i : i + 2]
-    if temp.count(C[i // 2]) == 0:
-        P += C[i // 2].lower()
-    elif temp.count(C[i // 2]) == 1 or temp.count(C[i // 2] + C[i // 2].lower()) == 2:
-        P += C[i // 2] + C[i // 2].lower()
-    else:
-        P += C[i // 2]
+import string
+
+res = {i : 0 for i in string.ascii_letters}
 for _ in range(int(input())):
-    flag = True
     for i in input():
-        if i not in P:
-            flag = False
-            break
-    print('Possible baby.' if flag else 'Not their baby!')
+        if i != ' ':
+            res[i] += 1
+for k, v in res.items():
+    if v:
+        print(k, v)
