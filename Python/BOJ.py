58c83a00b1f37f198cbb3d2a63b28db8ce14daab
right5625 <right5625@naver.com>
2024-04-28 23:53:23 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,10 +1,4 @@
-S = input()
-res = 0
-for i in range(0, len(S), 3):
-    if S[i] != 'P':
-        res += 1
-    if S[i + 1] != 'E':
-        res += 1
-    if S[i + 2] != 'R':
-        res += 1
-print(res)
+for _ in range(int(input())):
+    for i in list(input().split()):
+        print(i[::-1], end = ' ')
+    print()
