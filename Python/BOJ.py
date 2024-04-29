dd6d03a5b06691f250011fc0e9566f024986c3bd
right5625 <right5625@naver.com>
2024-04-29 12:41:46 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,4 +1,7 @@
-for _ in range(int(input())):
-    for i in list(input().split()):
-        print(i[::-1], end = ' ')
-    print()
+dic = {i : 0 for i in 'lkp'}
+for _ in range(3):
+    try:
+        dic[input()[0]] += 1
+    except:
+        pass
+print('GLOBAL' if list(dic.values()).count(1) == 3 else 'PONIX')
