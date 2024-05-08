8d745c6fe5abb7daeb482ed02eded9fc88a02c35
right5625 <right5625@naver.com>
2024-05-08 23:47:11 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,10 +1,2 @@
-x, y = map(int, input().split())
-day = y
-for i in range(1, x):
-    if i == 2:
-        day += 28
-    elif i in [4, 6, 9, 11]:
-        day += 30
-    else:
-        day += 31
-print(['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'][day % 7])
+a, b, c = map(int, input().split())
+print(c ** 2 - (max(a, b)) if c else int((a + b) ** 0.5))
