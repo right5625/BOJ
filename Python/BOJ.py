a92bcbb05d62091979a481d540616d14ff9fcacd
right5625 <right5625@naver.com>
2024-05-12 13:15:59 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,13 +1,15 @@
-n, k = map(int, input().split())
-cur = save = 0
-for _ in range(n):
-    s = input()
-    if s == 'save':
-        save = cur
-    elif s == 'load':
-        cur = save
-    elif s == 'shoot':
-        cur -= 1
-    else:
-        cur += k
-    print(cur)
+N, M = map(int, input().split())
+res = {}
+for _ in range(N):
+    A = input().split()
+    t = cur = 0
+    for i in A[:-1]:
+        if i == '.':
+            cur += 1
+        else:
+            t = max(t, cur)
+            cur = 0
+    res[A[-1]] = max(t, cur)
+print(len(set(res.values())))
+for k, v in res.items():
+    print(v, k)
