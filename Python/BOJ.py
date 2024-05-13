2af3224727aea8f180622b6825c466d8b91cb7b4
right5625 <right5625@naver.com>
2024-05-13 12:02:19 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,15 +1,2 @@
-N, M = map(int, input().split())
-res = {}
-for _ in range(N):
-    A = input().split()
-    t = cur = 0
-    for i in A[:-1]:
-        if i == '.':
-            cur += 1
-        else:
-            t = max(t, cur)
-            cur = 0
-    res[A[-1]] = max(t, cur)
-print(len(set(res.values())))
-for k, v in res.items():
-    print(v, k)
+input()
+print(min(int(''.join(input().split())), int(''.join(input().split()))))
