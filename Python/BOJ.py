5f8fbf0e050715077d5613c8605cc69cf6cd9360
right5625 <right5625@naver.com>
2024-05-24 13:02:34 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,2 +1,9 @@
-N, K = map(int, input().split())
-print(K // (2 ** (N - 1)))
+n = int(input())
+a = list(input().split())
+for i in range(n):
+    for j in range(n):
+        for k in range(1, min(len(a[i]), len(a[j])) + 1):
+            if a[i][:k] == a[j][-k:]:
+                print(i + 1, j + 1)
+                exit()
+print(-1)
