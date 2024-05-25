f321296fb13ffb15cf5642028c09b0382ee0a8e1
right5625 <right5625@naver.com>
2024-05-25 23:27:50 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,9 +1,2 @@
-n = int(input())
-a = list(input().split())
-for i in range(n):
-    for j in range(n):
-        for k in range(1, min(len(a[i]), len(a[j])) + 1):
-            if a[i][:k] == a[j][-k:]:
-                print(i + 1, j + 1)
-                exit()
-print(-1)
+s = input()
+print(abs(s.index('@') - s.index('#')) + abs(s.index('#') - s.index('!')) - 1 if s.replace('.', '') in ['@#!', '!#@'] else -1)
