9124331ffdac580bca7da6a9155a7e5049d3c9c3
right5625 <right5625@naver.com>
2024-05-26 11:56:38 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,2 +1,15 @@
-s = input()
-print(abs(s.index('@') - s.index('#')) + abs(s.index('#') - s.index('!')) - 1 if s.replace('.', '') in ['@#!', '!#@'] else -1)
+for _ in range(int(input())):
+    K = int(input())
+    exp = 0
+    while 2 ** exp < K:
+        exp += 1
+    if 2 ** exp == K:
+        print(0)
+    else:
+        cnt = 0
+        while K > 0:
+            exp -= 1
+            if K >= 2 ** exp:
+                K -= 2 ** exp
+            cnt += 1
+        print(cnt)
