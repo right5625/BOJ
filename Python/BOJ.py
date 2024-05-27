7a5b76de2bb3193cfe40eaa9470e71b9224ccee2
right5625 <right5625@naver.com>
2024-05-27 12:16:38 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,15 +1,2 @@
-for _ in range(int(input())):
-    K = int(input())
-    exp = 0
-    while 2 ** exp < K:
-        exp += 1
-    if 2 ** exp == K:
-        print(0)
-    else:
-        cnt = 0
-        while K > 0:
-            exp -= 1
-            if K >= 2 ** exp:
-                K -= 2 ** exp
-            cnt += 1
-        print(cnt)
+A, P, C = map(int, input().split())
+print(max(A + C, P))
