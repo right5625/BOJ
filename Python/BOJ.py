9a9880f62d4d89d4d329fc155d58ab81842fa9bc
right5625 <right5625@naver.com>
2024-05-29 18:10:51 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,6 +1,11 @@
-K = int(input())
-for i in ['G...', '.I.T', '..S.']:
-    for _ in range(K):
-        for j in i:
-            print(j * K, end = '')
-        print()
+while True:
+    n, m = map(int, input().split())
+    if n == m == 0:
+        break
+    nList, mList = [int(input()) for _ in range(n)], [int(input()) for _ in range(m)]
+    s1, s2 = sum(nList), sum(mList)
+    if (s1 - s2) % 2 == 1:
+        print(-1)
+    else:
+        res = [[i, j] for i in nList for j in mList if i - j == (s1 - s2) // 2]
+        print(-1) if not res else print(*sorted(res, key = lambda x : x[0] + x[1])[0])
