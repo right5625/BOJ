055458d68444840224cfa9a636cf4367b359f169
right5625 <right5625@naver.com>
2024-05-31 11:05:15 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,10 +1,5 @@
-while True:
-    C = int(input())
-    if not C:
-        break
-    res = [[1, C]]
-    for i in range(2, int(C ** 0.5) + 1):
-        if C % i == 0:
-            res.append([i, C // i])
-    res = sorted(res, key = lambda x : x[0] + x[1])[0]
-    print(f'Minimum perimeter is {(res[0] + res[1]) * 2} with dimensions {res[0]} x {res[1]}')
+import sys
+input = lambda : sys.stdin.readline().rstrip()
+
+for _ in range(int(input())):
+    print(int(input()) * 2 - 1)
