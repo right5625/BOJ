ecd84f26b4bde41db0f0a6b25e3f3a1d7df853f1
right5625 <right5625@naver.com>
2024-06-02 11:40:22 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,6 +1,5 @@
-import sys
-input = lambda : sys.stdin.readline().rstrip()
+def fac(n):
+    return 1 if n == 1 else n * fac(n - 1)
 
-face = [[0, 1, 2, 3], [0, 1, 4, 5], [0, 2, 4, 6], [1, 3, 5, 7], [2, 3, 6, 7], [4, 5, 6, 7]]
 for _ in range(int(input())):
-    print('YES' if sorted(list(map(int, input().split()))) in face else 'NO')
+    print(str(fac(int(input())))[-1])
