0f24c149e9977205c507b561f9ad3914fc56df50
right5625 <right5625@naver.com>
2024-06-04 14:46:28 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -2,8 +2,10 @@ import sys
 input = lambda : sys.stdin.readline().rstrip()
 
 N, Q = map(int, input().split())
-S = []
-for i in range(1, N + 1):
-    S.extend([i] * int(input()))
+L = list(map(int, input().split()))
 for _ in range(Q):
-    print(S[int(input())])
+    A, B, C = map(int, input().split())
+    if A == 1:
+        L[B - 1] = C
+    else:
+        print(abs(L[C - 1] - L[B - 1]))
