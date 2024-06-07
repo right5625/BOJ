ee8b4884878f12368ceb770eded905c7484d58ef
right5625 <right5625@naver.com>
2024-06-07 12:24:18 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,4 +1,12 @@
-for i in range(int(input())):
-    input()
-    count = list(input().split()).count('sheep')
-    print(f'Case {i + 1}: This list contains {count} sheep.\n')
+while True:
+    n = int(input())
+    if n == 0:
+        break
+    lst = [input() for _ in range(n)]
+    res = 0
+    while True:
+        lst = list(map(lambda x : x[1:], lst))
+        if len(set(lst)) != n or '' in lst:
+            break
+        res += 1
+    print(res)
