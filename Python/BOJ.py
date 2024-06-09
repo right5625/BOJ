affc3d560f42bbd70ab54e34ca9bc75cbdb0e8ec
right5625 <right5625@naver.com>
2024-06-09 11:54:56 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,3 +1,9 @@
-n = int(input())
-for i in range(n):
-    print(*[i * n + j for j in range(1, n + 1)]) if i % 2 == 0 else print(*[i * n + j for j in range(1, n + 1)][::-1])
+c, r = map(int, input().split())
+cur = [0, 0]
+while True:
+    a, b = map(int, input().split())
+    if a == b == 0:
+        break
+    cur[0] = min(max(cur[0] + a, 0), c)
+    cur[1] = min(max(cur[1] + b, 0), r)
+    print(*cur)
