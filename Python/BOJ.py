bafc96b22a91f34d9819785f9a8305e10b7b771e
right5625 <right5625@naver.com>
2024-06-10 12:15:40 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,9 +1,2 @@
-c, r = map(int, input().split())
-cur = [0, 0]
-while True:
-    a, b = map(int, input().split())
-    if a == b == 0:
-        break
-    cur[0] = min(max(cur[0] + a, 0), c)
-    cur[1] = min(max(cur[1] + b, 0), r)
-    print(*cur)
+N = input()
+print(N[:len(N) // 2], N[len(N) // 2:])
