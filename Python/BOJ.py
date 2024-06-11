2f1dacd783a827dee80c4bd61c45d58781faafdc
right5625 <right5625@naver.com>
2024-06-11 11:57:16 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,2 +1,9 @@
-N = input()
-print(N[:len(N) // 2], N[len(N) // 2:])
+import sys
+from collections import defaultdict
+input = lambda : sys.stdin.readline().rstrip()
+
+res = defaultdict(int)
+for _ in range(int(input())):
+    g, r = map(int, input().split())
+    res[r] = max(res[r], g)
+print(len(res))
