1fea225fb4ffc5ddcfb1190a17940b09e24d7a4f
right5625 <right5625@naver.com>
2024-06-13 14:21:01 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,6 +1,18 @@
-import string
-
-for i in [input() for _ in range(int(input()))]:
-    if sum([i.count(j) for j in string.ascii_uppercase]) <= sum([i.count(j) for j in string.ascii_lowercase]) and len(i) <= 10 and sum([i.count(j) for j in list(map(str, range(10)))]) != len(i):
-        print(i)
-        break
+N, M = map(int, input().split())
+S = [input() for _ in range(N)]
+for i in S:
+    lst = ' '.join(i.split('.')).split()
+    if not lst:
+        print(0)
+    else:
+        for j in lst:
+            print(len(j), end = ' ')
+        print()
+for i in list(zip(*S)):
+    lst = ' '.join(''.join(i).split('.')).split()
+    if not lst:
+        print(0)
+    else:
+        for j in lst:
+            print(len(j), end = ' ')
+        print()
