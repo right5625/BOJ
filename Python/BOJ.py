9f950127a27499f3929e51281986183494ebd666
right5625 <right5625@naver.com>
2024-06-14 15:07:34 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,18 +1,8 @@
-N, M = map(int, input().split())
-S = [input() for _ in range(N)]
-for i in S:
-    lst = ' '.join(i.split('.')).split()
-    if not lst:
-        print(0)
-    else:
-        for j in lst:
-            print(len(j), end = ' ')
-        print()
-for i in list(zip(*S)):
-    lst = ' '.join(''.join(i).split('.')).split()
-    if not lst:
-        print(0)
-    else:
-        for j in lst:
-            print(len(j), end = ' ')
-        print()
+x = int(input())
+m = int(input())
+res = 'No such integer exists.'
+for n in range(1, m):
+    if (x * n) % m == 1:
+        res = n
+        break
+print(res)
