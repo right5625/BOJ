d120cb133835a2c320791c9e88fd45f10d9d8f20
right5625 <right5625@naver.com>
2024-06-15 12:36:45 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,8 +1,24 @@
-x = int(input())
-m = int(input())
-res = 'No such integer exists.'
-for n in range(1, m):
-    if (x * n) % m == 1:
-        res = n
+while True:
+    n = int(input())
+    if not n:
         break
-print(res)
+    A = list(map(int, input().split()))
+    B = list(map(int, input().split()))
+    res = [0, 0]
+    for a, b in zip(A, B):
+        if b - a == 1:
+            if b == 2:
+                res[0] += 6
+            else:
+                res[0] += a + b
+        elif a - b == 1:
+            if a == 2:
+                res[1] += 6
+            else:
+                res[1] += a + b
+        elif a != b:
+            if a > b:
+                res[0] += a
+            else:
+                res[1] += b
+    print(f'A has {res[0]} points. B has {res[1]} points.\n')
