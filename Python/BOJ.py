13e9f639e3aed4ccf8876ccbf68a61f7547c2c13
Jinho <right5625@naver.com>
2024-06-16 13:34:14 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,24 +1,9 @@
-while True:
-    n = int(input())
-    if not n:
-        break
-    A = list(map(int, input().split()))
-    B = list(map(int, input().split()))
-    res = [0, 0]
-    for a, b in zip(A, B):
-        if b - a == 1:
-            if b == 2:
-                res[0] += 6
-            else:
-                res[0] += a + b
-        elif a - b == 1:
-            if a == 2:
-                res[1] += 6
-            else:
-                res[1] += a + b
-        elif a != b:
-            if a > b:
-                res[0] += a
-            else:
-                res[1] += b
-    print(f'A has {res[0]} points. B has {res[1]} points.\n')
+n = int(input())
+P = [list(map(int, input().split())) for _ in range(n)]
+for _ in range(int(input())):
+    i, d = map(int, input().split())
+    res = 0
+    for j in range(1, n + 1):
+        if ((P[i - 1][0] - P[j - 1][0]) ** 2 + (P[i - 1][1] - P[j - 1][1]) ** 2) ** 0.5 <= d:
+            res += 1
+    print(res - 1)
