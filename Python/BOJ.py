c9928dbcd109fab283f6c6e6b7c7d711e562fe48
Jinho <right5625@naver.com>
2024-06-18 11:16:34 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,5 +1,7 @@
-import string
-
-A = input()
-B = ''.join([i for i in input() if i in string.ascii_uppercase])
-print(*[chr(ord(B[i]) + (ord(A[i % len(A)]) - ord('A')) - 26) if chr(ord(B[i]) + (ord(A[i % len(A)]) - ord('A'))) > 'Z' else chr(ord(B[i]) + (ord(A[i % len(A)]) - ord('A'))) for i in range(len(B))], sep = '')
+N, X = map(int, input().split())
+res = -1
+for _ in range(N):
+    S, T = map(int, input().split())
+    if S + T <= X:
+        res = max(res, S)
+print(res)
