5e2007d42bb8d7e2ca17ee29dd433d3435ceb491
Jinho <right5625@naver.com>
2024-06-20 11:28:28 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,1 +1,9 @@
-print(sorted([list(input().split()) for _ in range(7)], key = lambda x : int(x[1]))[-1][0])
+M, DM = map(int, input().split())
+H, DH = map(int, input().split())
+ML = [max(M - DM * i, 0) for i in range(100)]
+HL = [max(H - DH * i, 0) for i in range(100)]
+res = 0
+for _ in range(int(input())):
+    C, B = map(int, input().split())
+    res += max(sum(ML[:C]), sum(HL[:B]))
+print(res)
