79eb0613b24f090cee92ec1e133569bf8a403385
Jinho <right5625@naver.com>
2024-06-23 11:51:26 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,12 +1,5 @@
-while True:
-    s1, s2 = input().split()
-    if s1 == s2 == '#':
-        break
-    res = [0, 0]
-    for _ in range(int(input())):
-        c1, c2 = input().split()
-        if c1 == c2:
-            res[0] += 1
-        else:
-            res[1] += 1
-    print(f'{s1} {res[0]} {s2} {res[1]}')
+for _ in range(int(input())):
+    res = 0
+    for i in list(''.join(list(input().split())[1:]).split('O')):
+        res = max(res, len(i))
+    print("The longest contiguous subsequence of X's is of length", res)
