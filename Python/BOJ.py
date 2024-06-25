e122194b04184ad275df4c511ea387ac5f3f356b
Jinho <right5625@naver.com>
2024-06-25 17:09:04 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,8 +1,8 @@
-import sys
-input = lambda : sys.stdin.readline().rstrip()
+from collections import defaultdict
 
-res = {i : 0 for i in range(1, 13)}
-for _ in range(int(input())):
-    res[int(input().split()[1].split('/')[1])] += 1
-for k, v in res.items():
-    print(k, v)
+res = defaultdict(int)
+N, M = map(int, input().split())
+for i in range(1, N + 1):
+    for j in range(1, M + 1):
+        res[i + j] += 1
+print(*[k for k, v in res.items() if v == max(list(res.values()))], sep = '\n')
