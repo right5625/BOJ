8a21c9e09cdf0e142c193cfe730dd387ec5507d9
Jinho <right5625@naver.com>
2024-06-28 22:34:28 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,10 +1,6 @@
-for i in range(int(input())):
-    C = int(input())
-    I = int(input())
-    P = list(map(int, input().split()))
-    flag = True
-    for j in range(I):
-        for k in range(I):
-            if flag and j != k and P[j] + P[k] == C:
-                print(f'Case #{i + 1}: {j + 1} {k + 1}')
-                flag = False
+S = list(input().split())
+dic = {'true' : True, 'false' : False}
+if S[1] == 'AND':
+    print('true' if dic[S[0]] and dic[S[2]] else 'false')
+else:
+    print('true' if dic[S[0]] or dic[S[2]] else 'false')
