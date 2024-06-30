1d309e012ecf4d5c097fccd8bd6d6892b1c742c8
Jinho <right5625@naver.com>
2024-06-30 10:11:46 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,16 +1,2 @@
-while True:
-    S = input()
-    if S == '0':
-        break
-    prev = S[0]
-    cnt = 1
-    res = ''
-    for cur in S[1:]:
-        if prev == cur:
-            cnt += 1
-        else:
-            res += str(cnt) + prev
-            prev = cur
-            cnt = 1
-    res += str(cnt) + prev
-    print(res)
+N, M = map(int, input().split())
+print(*sorted(set(map(int, input().split())).intersection(set(map(int, input().split())))), sep = '\n')
