9142067e39415988302e97804d4f78c5c0969c9b
Jinho <right5625@naver.com>
2024-07-05 19:50:05 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,10 +1,5 @@
-A = list(map(int, input().split()))
-B = list(map(int, input().split()))
-n, d = 0, 36
-for i in A:
-    for j in B:
-        if i > j:
-            n += 1
-        elif i == j:
-            d -= 1
-print(f'{n / d:.5f}')
+l, r = 9 * 60, 20 * 60 + 59
+for _ in range(int(input())):
+    h1, m1, h2, m2 = map(int, input().split())
+    l, r = max(l, h1 * 60 + m1), min(r, h2 * 60 + m2)
+print('NE' if l >= r else f'TAIP\n{l // 60} {l % 60} {r // 60} {r % 60}')
