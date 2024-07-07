642cebbb41b4c8ec567ce5459182600c180df766
Jinho <right5625@naver.com>
2024-07-07 11:32:21 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,6 +1,12 @@
-M, N = map(int, input().split())
-res = []
-for _ in range(N):
-    res.append(M % N + M // N)
-    M -= res[-1]
-print(f"{' '.join(map(str, sorted(res, reverse = True)))}\n{M}")
+import sys
+input = lambda : sys.stdin.readline().rstrip()
+
+while True:
+    N, C, K = map(int, input().split())
+    if N == C == K == 0:
+        break
+    dic = {i : 0 for i in range(1, K + 1)}
+    for _ in range(N):
+        for i in list(map(int, input().split())):
+            dic[i] += 1
+    print(*list(map(lambda x : x[0], filter(lambda x : x[1] == min(dic.values()), sorted(dic.items(), key = lambda x : (x[1], x[0]))))))
