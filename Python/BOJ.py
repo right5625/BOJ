fac095d8260b56dbc544857d75e7438def686877
Jinho <right5625@naver.com>
2024-07-08 11:44:49 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,12 +1,3 @@
-import sys
-input = lambda : sys.stdin.readline().rstrip()
-
-while True:
-    N, C, K = map(int, input().split())
-    if N == C == K == 0:
-        break
-    dic = {i : 0 for i in range(1, K + 1)}
-    for _ in range(N):
-        for i in list(map(int, input().split())):
-            dic[i] += 1
-    print(*list(map(lambda x : x[0], filter(lambda x : x[1] == min(dic.values()), sorted(dic.items(), key = lambda x : (x[1], x[0]))))))
+for _ in range(int(input())):
+    s = sum([int(S) * int(R) for _ in range(int(input())) for S, R in [input().split()]])
+    print(*[i // s for i in [1296, 2592, 3888]])
