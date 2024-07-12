d2105199a27f12f0673ef94f04906a106a2ec0b6
Jinho <right5625@naver.com>
2024-07-12 19:59:59 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,14 +1,5 @@
-A = list(map(int, input().split()))
-B = list(map(int, input().split()))
-if sum(A) == sum(B):
-    res = 'remis'
-    for i in range(10, -1, -1):
-        if A.count(i) > B.count(i):
-            res = 'Algosia'
-            break
-        elif A.count(i) < B.count(i):
-            res = 'Bajtek'
-            break
-    print(res)
-else:
-    print('Algosia' if sum(A) > sum(B) else 'Bajtek')
+while True:
+    K, J, T, D = map(int, input().split())
+    if K == J == T == D == 0:
+        break
+    print('yes' if K + 0.5 > max(J - 0.5, 0) * 9 + max(T - 0.5, 0) * 4 + max(D - 0.5, 0) * 4 and max(K - 0.5, 0) < (J + 0.5) * 9 + (T + 0.5) * 4 + (D + 0.5) * 4 else 'no')
