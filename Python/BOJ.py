937562215722a606e9fb3f0dd7e9a422c9eda8d7
Jinho <right5625@naver.com>
2024-07-19 23:29:52 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,4 +1,8 @@
-L, C, N = map(int, input().split())
-for _ in range(C):
-    S, P = map(int, input().split())
-    print(abs(N - P) + S)
+X, Y, I = map(int, input().split())
+S = [[False for _ in range(X)] for _ in range(Y)]
+for _ in range(I):
+    Xl, Yl, Xu, Yu = map(int, input().split())
+    for i in range(Yl - 1, Yu):
+        for j in range(Xl - 1, Xu):
+            S[i][j] = True
+print(sum([i.count(True) for i in S]))
