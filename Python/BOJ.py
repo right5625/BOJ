ac571f9fdac7b6849fa22e5fa5507281c0379d6f
Jinho <right5625@naver.com>
2024-07-22 12:14:59 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,8 +1,5 @@
-a = int(input())
-b = int(input())
-n, res = 1, 0
-while n ** 6 <= b:
-    if a <= n ** 6:
-        res += 1
-    n += 1
-print(res)
+while True:
+    n, m, c = map(int, input().split())
+    if n == m == c == 0:
+        break
+    print(((n - 7) // 2) * ((m - 7) // 2) + ((n - 6) // 2) * ((m - 6) // 2) if c == 1 else ((n - 7) // 2) * ((m - 6) // 2) + ((n - 6) // 2) * ((m - 7) // 2))
