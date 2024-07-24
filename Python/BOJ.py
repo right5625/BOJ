51d78e0d5e12009eed2fff0ab1521a457afa9403
Jinho <right5625@naver.com>
2024-07-24 11:44:04 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,3 +1,8 @@
 for _ in range(int(input())):
-    N, A, D = map(int, input().split())
-    print(sum([A + D * i for i in range(N)]))
+    words = [input() for _ in range(3)]
+    res = 'Yes'
+    for i in range(3):
+        for j in range(3):
+            if i != j and (words[i].startswith(words[j]) or words[i].endswith(words[j])):
+                res = 'No'
+    print(res)
