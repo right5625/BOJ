b72c73f4644310ab4ba28679655f8254a9fa0598
Jinho <right5625@naver.com>
2024-07-26 13:21:04 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,10 +1,3 @@
 for _ in range(int(input())):
-    n = int(input())
-    a = list(map(int, input().split()))
-    b = list(map(int, input().split()))
-    res = 0
-    for i in range(n):
-        for j in range(i + 1, n):
-            if a[i] <= b[j]:
-                res = max(res, abs(i - j))
-    print(f'The maximum distance is {res}\n')
+    x = list(map(int, input().split()))
+    print(f'The sequence {x[1:]} is not an arithmetic sequence.' if x[0] == 1 or len(set([x[i] - x[i - 1] for i in range(2, x[0] + 1)])) != 1 else f'The next 5 numbers after {x[1:]} are: {[i for i in range(x[-1] + (x[-1] - x[-2]), x[-1] + (x[-1] - x[-2]) * 6, x[-1] - x[-2])]}')
