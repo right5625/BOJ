8e521aebdbcf9338649aa704038cfa1f3b5baf11
Jinho <right5625@naver.com>
2024-07-27 11:23:27 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,3 +1,3 @@
-for _ in range(int(input())):
-    x = list(map(int, input().split()))
-    print(f'The sequence {x[1:]} is not an arithmetic sequence.' if x[0] == 1 or len(set([x[i] - x[i - 1] for i in range(2, x[0] + 1)])) != 1 else f'The next 5 numbers after {x[1:]} are: {[i for i in range(x[-1] + (x[-1] - x[-2]), x[-1] + (x[-1] - x[-2]) * 6, x[-1] - x[-2])]}')
+n = int(input())
+a = list(map(int, input().split()))
+print(max(abs(a.index(0) - (n - 1 - a[::-1].index(1))), abs(a.index(1) - (n - 1 - a[::-1].index(0)))))
