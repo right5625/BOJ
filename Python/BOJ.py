1da0e173c03a36822fa260d0bab0e07b20ff6532
Jinho <right5625@naver.com>
2024-07-29 11:32:12 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,12 +1,4 @@
-N, M = map(int, input().split())
-time, res = [0] * N, [0] * N
-for _ in range(M):
-    s = list(map(int, input().split()))
-    t, v, z = s[0], s[1], s[2:]
-    for i in sorted(z):
-        idx = z.index(i)
-        if t >= time[idx]:
-            res[idx] += v
-            time[idx] = t + i
-            break
-print(*res)
+from math import ceil
+
+a, b, h, w = map(int, input().split())
+print(ceil(h / a) * ceil(w / b))
