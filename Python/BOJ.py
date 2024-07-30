2efbfe31e5ec309a54dae87b710e03d077b814c4
Jinho <right5625@naver.com>
2024-07-30 14:43:46 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,4 +1,4 @@
-from math import ceil
-
-a, b, h, w = map(int, input().split())
-print(ceil(h / a) * ceil(w / b))
+N = int(input())
+A = list(map(int, input().split()))
+lst = list(map(lambda x : min(x), [[abs(N - (i * (N // i))), abs(N - (i * (N // i + 1)))] for i in A]))
+print(f'{A[lst.index(min(lst))]} {N - min(lst) if (N - min(lst)) % A[lst.index(min(lst))] == 0 else N + min(lst)}')
