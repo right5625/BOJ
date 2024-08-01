9194783418c53f2cade19a81a3a4fa1e3e042050
Jinho <right5625@naver.com>
2024-08-01 11:27:56 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,7 +1,5 @@
-from collections import defaultdict
-
-n, m, k, a, b, c = map(int, input().split())
-dic = defaultdict(list)
-for i, j in zip([n * a, m * b, k * c], ['Joffrey', 'Robb', 'Stannis']):
-    dic[i].append(j)
-print(*dic[max(dic)])
+while True:
+    s = list(input().split())
+    if s[0] == '#':
+        break
+    print(*[i if len(i) == 1 else i[0] + i[1 : -1][::-1] + i[-1] for i in s])
