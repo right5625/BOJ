434f11e930d219261e75d62c106db19833bc5683
Jinho <right5625@naver.com>
2024-08-02 13:24:54 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,5 +1,17 @@
-while True:
-    s = list(input().split())
-    if s[0] == '#':
-        break
-    print(*[i if len(i) == 1 else i[0] + i[1 : -1][::-1] + i[-1] for i in s])
+M, N = map(int, input().split())
+G = [list(map(int, input())) for _ in range(M)]
+res = 0
+for i in range(M):
+    for j in range(N):
+        diff = []
+        if i > 0:
+            diff.append(abs(G[i][j] - G[i - 1][j]))
+        if i < M - 1:
+            diff.append(abs(G[i][j] - G[i + 1][j]))
+        if j > 0:
+            diff.append(abs(G[i][j] - G[i][j - 1]))
+        if j < N - 1:
+            diff.append(abs(G[i][j] - G[i][j + 1]))
+        if diff:
+            res += sum(diff) / len(diff)
+print(f'{res:.4f}')
