09c56fc3b7a8f9117a538938d6a08505a3b3c022
Jinho <right5625@naver.com>
2024-08-03 13:48:11 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,17 +1,4 @@
-M, N = map(int, input().split())
-G = [list(map(int, input())) for _ in range(M)]
-res = 0
-for i in range(M):
-    for j in range(N):
-        diff = []
-        if i > 0:
-            diff.append(abs(G[i][j] - G[i - 1][j]))
-        if i < M - 1:
-            diff.append(abs(G[i][j] - G[i + 1][j]))
-        if j > 0:
-            diff.append(abs(G[i][j] - G[i][j - 1]))
-        if j < N - 1:
-            diff.append(abs(G[i][j] - G[i][j + 1]))
-        if diff:
-            res += sum(diff) / len(diff)
-print(f'{res:.4f}')
+N, K, M = map(int, input().split())
+G = [list(map(int, input().split()))[1:] for _ in range(K)]
+C = list(map(int, input().split()))
+print(sum([-(-sum(C[j - 1] for j in i) // M) for i in G]))
