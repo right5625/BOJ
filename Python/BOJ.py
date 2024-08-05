a0c57048c50e2a37b7792cdc0ebc940f58826959
Jinho <right5625@naver.com>
2024-08-05 14:21:27 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,9 +1,8 @@
-from collections import defaultdict
-
-for _ in range(int(input())):
-    dic = defaultdict(int)
-    for _ in range(int(input())):
-        s, a, b = input().split()
-        for i in range(int(a), int(b)):
-            dic[i] += 1
-    print(''.join(chr(v + ord('A') - 1) for _, v in sorted(dic.items())))
+while True:
+    n, f = input().split()
+    if n == '#':
+        break
+    print(f'{n} Library')
+    for i in range(int(input())):
+        w, s = input().split()
+        print(f'Book {i + 1} {"horizontal" if int(w) >= int(f) * len(s) + 2 else "vertical"}')
