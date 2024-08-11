266179334d130aa07c3df4924e139b5a29d1a470
Jinho <right5625@naver.com>
2024-08-11 12:49:02 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,21 +1,2 @@
-from collections import defaultdict
-
-codeDic = {'L' : 120, 'S' : 150, 'B' : 150, 'N' : 40, 'C' : 160, 'D' : 100, 'R' : 100, 'O' : 100}
-while True:
-    name = input()
-    if name == '#':
-        break
-    dic = defaultdict(int)
-    upgrade = 0
-    while True:
-        line = input()
-        if line == '00A':
-            break
-        seat, code = line.split()
-        try:
-            dic[seat] += codeDic[code]
-            if dic[seat] >= 200:
-                upgrade += 1
-        except:
-            pass
-    print(f'{name} {upgrade}')
+n = int(input())
+print(1 if n == 0 else {1 : 2, 2 : 4, 3 : 8, 0 : 6}[n % 4])
