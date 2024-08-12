b8fc4573d5e1c62e6811ac156df388a6126d1783
Jinho <right5625@naver.com>
2024-08-12 13:01:10 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,2 +1,13 @@
-n = int(input())
-print(1 if n == 0 else {1 : 2, 2 : 4, 3 : 8, 0 : 6}[n % 4])
+import sys
+input = lambda : sys.stdin.readline().rstrip()
+
+flag = True
+prevA = prevB = 0
+for i in range(int(input())):
+    A, B = map(int, input().split())
+    if abs(A - B) % 12 == 7 and abs(prevA - prevB) % 12 == 7 and A != prevA and B != prevB:
+        print(i)
+        flag = False
+    prevA, prevB = A, B
+if flag:
+    print('POLE')
