ef4c60a7e4db219c5aa6ceb9f649d7e8aee4bc31
Jinho <right5625@naver.com>
2024-08-17 13:30:24 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,5 +1,5 @@
-n, m = map(int, input().split())
-task = [list(map(int, input().split())) for _ in range(n)]
-weights = [1 if task[0][i] else 0 for i in range(m)]
-scores = [sum([weights[j] * task[i][j] for j in range(m)]) for i in range(n)]
-print(1, scores.count(scores[0]))
+N = int(input())
+S = input()
+while 'PS4' in S or 'PS5' in S:
+    S = S.replace('PS4', 'PS').replace('PS5', 'PS')
+print(S)
