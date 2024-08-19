9292bbcd47634b4d27d1f92ac94864fa3ec444e1
Jinho <right5625@naver.com>
2024-08-19 12:19:45 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,6 +1,5 @@
-import sys
-input = lambda : sys.stdin.readline().rstrip()
-
 for _ in range(int(input())):
-    n, k = map(int, input().split())
-    print(min(n, k - 1))
+    N = int(input())
+    S = input()
+    K = (ord(input()) - ord(S[0]) + 26) % 26
+    print(''.join([chr((ord(i) - ord('a') + K) % 26 + ord('a')) for i in S]))
