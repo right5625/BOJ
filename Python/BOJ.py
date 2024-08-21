4e4590400043f2ec39dc3c1f1d70720789081958
Jinho <right5625@naver.com>
2024-08-21 12:30:17 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,5 +1,6 @@
-for _ in range(int(input())):
-    N = int(input())
-    S = input()
-    K = (ord(input()) - ord(S[0]) + 26) % 26
-    print(''.join([chr((ord(i) - ord('a') + K) % 26 + ord('a')) for i in S]))
+A = int(input())
+inc, res = 1, 0
+for _ in range(A):
+    res = (res + inc) % 500000009
+    inc = inc * 4 % 500000009
+print(res)
