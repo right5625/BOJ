936e10e44f0dab411fd6a7fe06ddfec336cfd1bb
Jinho <right5625@naver.com>
2024-08-25 14:57:02 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,3 +1,3 @@
 for _ in range(int(input())):
-    S = input()
-    print(sorted([S[i:] + S[:i] for i in range(len(S))])[0])
+    a, b = map(set, input().split())
+    print("YES" if a == b else "NO")
