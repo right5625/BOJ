8c4e58b793fb72cbf6ebb9731d98379fa90a96c1
Jinho <right5625@naver.com>
2024-08-26 15:37:58 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,3 +1,21 @@
-for _ in range(int(input())):
-    a, b = map(set, input().split())
-    print("YES" if a == b else "NO")
+while True:
+    n = int(input())
+    if n == 0:
+        break
+    res = 0
+    for i in range(1, n + 1):
+        if i**3 > n:
+            break
+        res = max(res, i**3)
+    for i in range(1, n + 1):
+        if i * (i + 1) * (i + 2) // 6 > n:
+            break
+        res = max(res, i * (i + 1) * (i + 2) // 6)
+    for i in range(1, n + 1):
+        if i**3 > n:
+            break
+        for j in range(1, n + 1):
+            if i**3 + j * (j + 1) * (j + 2) // 6 > n:
+                break
+            res = max(res, i**3 + j * (j + 1) * (j + 2) // 6)
+    print(res)
