75580a9e891b2d9d6382fe7faa03947127beb122
Jinho <right5625@naver.com>
2024-08-28 13:36:06 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,3 +1,10 @@
 for _ in range(int(input())):
-    N, K = map(int, input().split())
-    print((N - K) // (K - 1))
+    m, n = map(int, input().split())
+    a = list(map(int, input().split()))
+    if 3 in a:
+        a = a[::-1]
+        a[a.index(3)] = 2
+    b = a[a.index(2) :].copy()
+    while len(b) < n + 1:
+        b.extend(a[::-1][1:] + a[1:])
+    print(b[: n + 1].count(0))
