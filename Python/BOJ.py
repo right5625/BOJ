ab190e47ccda006f2d6e6efa00f8fffbd9b351da
Jinho <right5625@naver.com>
2024-08-29 13:26:33 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,10 +1,9 @@
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+
+res = 0
 for _ in range(int(input())):
-    m, n = map(int, input().split())
-    a = list(map(int, input().split()))
-    if 3 in a:
-        a = a[::-1]
-        a[a.index(3)] = 2
-    b = a[a.index(2) :].copy()
-    while len(b) < n + 1:
-        b.extend(a[::-1][1:] + a[1:])
-    print(b[: n + 1].count(0))
+    A, B = map(int, input().split())
+    res += A - B
+    print(res)
