2a3347c7eb9fd74902545a56e05b013da5da915e
Jinho <right5625@naver.com>
2024-08-30 14:14:32 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,9 +1,10 @@
-import sys
+import re
 
-input = lambda: sys.stdin.readline().rstrip()
-
-res = 0
 for _ in range(int(input())):
-    A, B = map(int, input().split())
-    res += A - B
-    print(res)
+    S = input()
+    res = float("inf")
+    for i in range(len(S) - 5):
+        for j in range(i + 6, len(S) + 1):
+            if re.match(r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)", S[i:j]):
+                res = min(res, j - i)
+    print(res if res != float("inf") else 0)
