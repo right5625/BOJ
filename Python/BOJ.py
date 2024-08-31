b87ce2ca24822ff84d7f8124f4f7a51f1572c49c
Jinho <right5625@naver.com>
2024-08-31 13:06:14 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,10 +1,7 @@
-import re
-
 for _ in range(int(input())):
-    S = input()
-    res = float("inf")
-    for i in range(len(S) - 5):
-        for j in range(i + 6, len(S) + 1):
-            if re.match(r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)", S[i:j]):
-                res = min(res, j - i)
-    print(res if res != float("inf") else 0)
+    A = list(map(int, input().split()))
+    for i in range(19, 0, -1):
+        if A[i] >= 2:
+            A[i - 1] += A[i] // 2
+        A[i] = A[i] % 2
+    print(*A)
