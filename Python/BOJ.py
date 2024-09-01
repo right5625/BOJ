68f93266241102f1039a1bcdc157f09f4a64573a
Jinho <right5625@naver.com>
2024-09-01 11:15:06 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,7 +1,9 @@
-for _ in range(int(input())):
-    A = list(map(int, input().split()))
-    for i in range(19, 0, -1):
-        if A[i] >= 2:
-            A[i - 1] += A[i] // 2
-        A[i] = A[i] % 2
-    print(*A)
+A, B = map(int, input().split())
+cnt = 0
+if A % 2 == 0:
+    A += 1
+    cnt += 1
+if B % 2 == 1:
+    B -= 1
+    cnt += 1
+print(cnt + (B - A + 1) // 2)
