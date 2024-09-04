a5603c307220687185e55de03e4d6205e07be36f
Jinho <right5625@naver.com>
2024-09-04 13:09:21 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,14 +1,11 @@
-print(
-    {
-        1: "11\nA B C D E F G H J L M",
-        2: "9\nA C E F G H I L M",
-        3: "9\nA C E F G H I L M",
-        4: "9\nA B C E F G H L M",
-        5: "8\nA C E F G H L M",
-        6: "8\nA C E F G H L M",
-        7: "8\nA C E F G H L M",
-        8: "8\nA C E F G H L M",
-        9: "8\nA C E F G H L M",
-        10: "8\nA B C F G H L M",
-    }[int(input())]
-)
+cur = 0
+res = "See you next month"
+for _ in range(int(input())):
+    x, y = map(int, input().split())
+    if x == 1:
+        cur += y
+    else:
+        cur -= y
+        if cur < 0:
+            res = "Adios"
+print(res)
