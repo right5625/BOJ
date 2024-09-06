5448537d585bd1b41787f5d38eba22c8e9eca524
Jinho <right5625@naver.com>
2024-09-06 12:44:46 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,13 +1,2 @@
-S = input()
-res = []
 for _ in range(int(input())):
-    s = input()
-    flag = True
-    for i, j in zip(S, s):
-        if i != "*" and i != j:
-            flag = False
-            break
-    if flag:
-        res.append(s)
-print(len(res))
-print(*res, sep="\n")
+    print("YES" if sum(list(map(int, input()))) % 9 == 0 else "NO")
