7c77d2283a0be065afc6ddc805fcafb4e60b5b30
Jinho <right5625@naver.com>
2024-09-08 23:09:34 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,19 +1,2 @@
-for _ in range(int(input())):
-    cnt = 0
-    res = [0, 0, 0, 0]
-    for i in input():
-        if i == "U":
-            res[1] += 1
-            res[3] += 1
-        elif i == "R":
-            res[0] += 1
-            res[2] += 1
-        elif i == "D":
-            res[1] -= 1
-            res[3] -= 1
-        elif i == "L":
-            res[0] -= 1
-            res[2] -= 1
-        else:
-            cnt += 1
-    print(res[0] - cnt, res[1] - cnt, res[2] + cnt, res[3] + cnt)
+n, m, k = map(int, input().split())
+print(m * k + m)
