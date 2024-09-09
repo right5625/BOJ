488c19f2cf961ee228f12c0c30d263bb4be57335
Jinho <right5625@naver.com>
2024-09-09 13:06:33 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,2 +1,13 @@
-n, m, k = map(int, input().split())
-print(m * k + m)
+n, k, T0 = map(int, input().split())
+T = [T0]
+print(
+    sum(
+        [
+            abs(i - k)
+            for i in [
+                T[-1] + i - abs(T[-1] - k) if T[-1] > k else T[-1] + i + abs(T[-1] - k)
+                for i in list(map(int, input().split()))
+            ]
+        ]
+    )
+)
