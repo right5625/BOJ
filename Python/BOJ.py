466acaccc543c089b41c47d2f3cf9b6c4b56843a
Jinho <right5625@naver.com>
2024-09-10 11:38:26 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,13 +1,2 @@
-n, k, T0 = map(int, input().split())
-T = [T0]
-print(
-    sum(
-        [
-            abs(i - k)
-            for i in [
-                T[-1] + i - abs(T[-1] - k) if T[-1] > k else T[-1] + i + abs(T[-1] - k)
-                for i in list(map(int, input().split()))
-            ]
-        ]
-    )
-)
+N = int(input())
+print(N if N != 2 else 3)
