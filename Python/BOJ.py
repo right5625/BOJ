ce2c16cb35107b71810d59ff55868eaf2d95891d
Jinho <right5625@naver.com>
2024-09-16 12:52:05 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,18 +1,3 @@
-for i in range(int(input())):
-    m = int(input())
-    a = []
-    while len(a) != m * 2:
-        a.extend(list(map(int, input().split())))
-    res = [0, 0]
-    for j in range(0, m * 2, 2):
-        if a[j] > a[j + 1]:
-            if a[j] - a[j + 1] == 1:
-                res[1] += a[j] + a[j + 1] if a[j] != 2 else 6
-            else:
-                res[0] += a[j]
-        elif a[j + 1] > a[j]:
-            if a[j + 1] - a[j] == 1:
-                res[0] += a[j + 1] + a[j] if a[j + 1] != 2 else 6
-            else:
-                res[1] += a[j + 1]
-    print(f"Game {i + 1}: Tessa {res[0]} Danny {res[1]}")
+a = int(input())
+w, v = map(int, input().split())
+print(1 if a <= w // v else 0)
