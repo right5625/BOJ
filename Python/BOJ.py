bfecfda8b1fc9a8e64bc8e48021b89a95a1212fa
Jinho <right5625@naver.com>
2024-09-18 11:35:09 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,3 +1,5 @@
-a = sum([(i + 1) * j for i, j in enumerate(list(map(int, input().split())))])
-b = sum([(i + 1) * j for i, j in enumerate(list(map(int, input().split())))])
-print(0) if a == b else print(1 if a > b else 2)
+from math import ceil
+
+a, b = map(int, input().split())
+d = int(input())
+print(ceil(a * b / 12) * d)
