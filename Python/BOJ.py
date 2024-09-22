dbd47dc0eba76f24a0e8d09f9f7ee5a4771aae9a
Jinho <right5625@naver.com>
2024-09-22 11:30:31 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,11 +1,2 @@
-import sys
-
-input = lambda: sys.stdin.readline().rstrip()
-
 N = int(input())
-H = [int(input()) for _ in range(N)]
-res = 0
-for i in range(N // 2):
-    if H[i] == H[i + N // 2]:
-        res += 2
-print(res)
+print("YES" if "gori" in input() else "NO")
