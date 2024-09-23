d3206bd613691fef18c677d142921bfe2150c059
Jinho <right5625@naver.com>
2024-09-23 11:49:15 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,2 +1,7 @@
 N = int(input())
-print("YES" if "gori" in input() else "NO")
+if -(2**15) <= N <= 2**15 - 1:
+    print("short")
+elif -(2**31) <= N <= 2**31 - 1:
+    print("int")
+else:
+    print("long long")
