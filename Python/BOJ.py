502688c914acb69ae8cdd2fc6b9bc7c3fe1bcca7
Jinho <right5625@naver.com>
2024-09-25 10:56:42 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,3 +1,3 @@
-for _ in range(int(input())):
-    S = input()
-    print([S[i : i + 3] == "WOW" for i in range(len(S) - 2)].count(True))
+x, y = map(int, input().split())
+x, y = bin(x)[2:].zfill(16), bin(y)[2:].zfill(16)
+print(int("".join([x[i] if j == 0 else y[i] for i in range(16) for j in range(2)]), 2))
