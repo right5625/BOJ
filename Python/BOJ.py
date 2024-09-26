5e718b789f96ef813a94e848678e8583d70d9bf8
Jinho <right5625@naver.com>
2024-09-26 11:44:41 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,3 +1,4 @@
-x, y = map(int, input().split())
-x, y = bin(x)[2:].zfill(16), bin(y)[2:].zfill(16)
-print(int("".join([x[i] if j == 0 else y[i] for i in range(16) for j in range(2)]), 2))
+A, B = map(int, input().split())
+while A != B:
+    A, B = abs(A - B), min(A, B)
+print(A)
