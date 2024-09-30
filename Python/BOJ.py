fb0a9ce2175d818bf1649e3054d499ce381b3dcd
Jinho <right5625@naver.com>
2024-09-30 10:02:10 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,2 +1,10 @@
-N, K = int(input()), int(input())
-print((K - (N - 1)) // N)
+N, A, B = map(int, input().split())
+res1, res2 = 1, 1
+for _ in range(N):
+    res1 += A
+    res2 += B
+    if res1 < res2:
+        res1, res2 = res2, res1
+    elif res1 == res2:
+        res2 -= 1
+print(res1, res2)
