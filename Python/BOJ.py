c487d937d64e790eabb66bd33b98688e3af3f0d6
Jinho <right5625@naver.com>
2024-10-02 12:55:58 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,2 +1,31 @@
-S = input()
-print(0) if S == "(1)" else print(2 if ")1(" in S else 1)
+month = {
+    i: j
+    for i, j in zip(
+        [
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "August",
+            "September",
+            "October",
+            "November",
+            "December",
+        ],
+        range(1, 13),
+    )
+}
+while True:
+    d, m = input().split()
+    d = int(d)
+    if d == 0:
+        break
+    if month[m] == 8 and d == 4:
+        print("Happy birthday")
+    elif month[m] == 2 and d == 29:
+        print("Unlucky")
+    else:
+        print("Yes" if month[m] < 8 or (month[m] == 8 and d < 4) else "No")
