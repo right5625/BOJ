63a4a9d0eb0aea88b3109bba5844c61f1eacf94d
Jinho <right5625@naver.com>
2024-10-06 11:46:51 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,18 +1,1 @@
-import string
-
-while True:
-    S = input()
-    if S == "#":
-        break
-    res = ""
-    for i in S[:-1]:
-        if i not in string.ascii_letters:
-            res += i
-        else:
-            s = chr(ord(i) - (ord(S[-1]) - ord("A")))
-            if (i in string.ascii_lowercase and s not in string.ascii_lowercase) or (
-                i in string.ascii_uppercase and s not in string.ascii_uppercase
-            ):
-                s = chr(ord(s) + 26)
-            res += s
-    print(res)
+print(*(["LoveisKoreaUniversity"] * int(input())), sep=" ")
