7d8485ce4954285a0bc5a4fc682e9c71fba685a8
Jinho <right5625@naver.com>
2024-10-11 12:58:10 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,15 +1,7 @@
-import sys
-
-input = lambda: sys.stdin.readline().rstrip()
-
-dic = {i: 0 for i in range(1, 51)}
-n = int(input())
-for _ in range(n):
-    for __ in range(10):
-        for j in list(map(int, input().split())):
-            dic[j] += 1
-res = []
-for k, v in dic.items():
-    if v > n * 2:
-        res.append(k)
-print(*res) if res else print(-1)
+for _ in range(int(input())):
+    a, b, c = map(int, input().split())
+    res = set()
+    for i in range(1, a):
+        if i % b == 0 or i % c == 0:
+            res.add(i)
+    print(f"{a} {b} {c}\n{len(res)}")
