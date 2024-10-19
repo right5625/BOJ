a5337ba16718cacdd7b1e25d9070dcb97a399a1a
Jinho <right5625@naver.com>
2024-10-19 11:33:29 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,3 +1,16 @@
-for _ in range(int(input())):
-    w = input()
-    print(f"The number of vowels in {w} is {sum([w.count(i) for i in 'aeiou'])}.")
+import string
+
+while True:
+    S = input()
+    if S == "0":
+        break
+    N, S = S.split()
+    res = ""
+    for i in S[::-1]:
+        s = ord(i) - ord("A") + int(N)
+        if i == "_":
+            s -= 4
+        elif i == ".":
+            s += 46
+        res += (string.ascii_uppercase + "_.")[s % 28]
+    print(res)
