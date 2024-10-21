91ba30608fc48cd3954e0ea964b283bdc6bae0de
Jinho <right5625@naver.com>
2024-10-21 10:37:56 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,7 +1,8 @@
-S = []
-while True:
-    try:
-        S.append(len(input()))
-    except:
-        break
-print(sum((max(S) - i) ** 2 for i in S[:-1]))
+for _ in range(int(input())):
+    n, s, b, c = input().split()
+    if int(s.split("/")[0]) >= 2010 or int(b.split("/")[0]) >= 1991:
+        print(n, "eligible")
+    elif int(c) > 40:
+        print(n, "ineligible")
+    else:
+        print(n, "coach petitions")
