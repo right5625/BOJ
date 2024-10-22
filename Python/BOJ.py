525c4f18feb4dcc4516c33815b83891398509cfb
Jinho <right5625@naver.com>
2024-10-22 10:25:27 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,8 +1,5 @@
-for _ in range(int(input())):
-    n, s, b, c = input().split()
-    if int(s.split("/")[0]) >= 2010 or int(b.split("/")[0]) >= 1991:
-        print(n, "eligible")
-    elif int(c) > 40:
-        print(n, "ineligible")
-    else:
-        print(n, "coach petitions")
+while True:
+    try:
+        print("yes" if "problem" in input().lower() else "no")
+    except:
+        break
