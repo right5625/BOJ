d05632aabb5a129a32f68bbfe22f3680224467ce
Jinho <right5625@naver.com>
2024-10-23 10:37:28 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,5 +1,6 @@
-while True:
-    try:
-        print("yes" if "problem" in input().lower() else "no")
-    except:
-        break
+X_lst, Y_lst = [], []
+for _ in range(int(input())):
+    X, Y = map(int, input().split())
+    X_lst.append(X)
+    Y_lst.append(Y)
+print(max(max(X_lst) - min(X_lst), max(Y_lst) - min(Y_lst)) ** 2)
