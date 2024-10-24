dba3639ae2d0c92c3d4260795e4f2beff7227808
Jinho <right5625@naver.com>
2024-10-24 20:27:45 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,6 +1,19 @@
-X_lst, Y_lst = [], []
-for _ in range(int(input())):
-    X, Y = map(int, input().split())
-    X_lst.append(X)
-    Y_lst.append(Y)
-print(max(max(X_lst) - min(X_lst), max(Y_lst) - min(Y_lst)) ** 2)
+res = [0] * 8
+for i in input():
+    if i in "1QAZ":
+        res[0] += 1
+    elif i in "2WSX":
+        res[1] += 1
+    elif i in "3EDC":
+        res[2] += 1
+    elif i in "4RFV5TGB":
+        res[3] += 1
+    elif i in "6YHN7UJM":
+        res[4] += 1
+    elif i in "8IK,":
+        res[5] += 1
+    elif i in "9OL.":
+        res[6] += 1
+    else:
+        res[7] += 1
+print(*res, sep="\n")
