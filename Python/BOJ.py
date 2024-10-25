d6b109a06038b82086dd03611a0f387447c4bc77
Jinho <right5625@naver.com>
2024-10-25 11:19:58 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,19 +1,3 @@
-res = [0] * 8
-for i in input():
-    if i in "1QAZ":
-        res[0] += 1
-    elif i in "2WSX":
-        res[1] += 1
-    elif i in "3EDC":
-        res[2] += 1
-    elif i in "4RFV5TGB":
-        res[3] += 1
-    elif i in "6YHN7UJM":
-        res[4] += 1
-    elif i in "8IK,":
-        res[5] += 1
-    elif i in "9OL.":
-        res[6] += 1
-    else:
-        res[7] += 1
-print(*res, sep="\n")
+S = [int(input()) for _ in range(8)]
+S += S[:3]
+print(max([sum(S[i : i + 4]) for i in range(8)]))
