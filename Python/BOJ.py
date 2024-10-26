194805fc50a00a0d979747bf06bfdc5922fbb7c7
Jinho <right5625@naver.com>
2024-10-26 21:17:20 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,3 +1,3 @@
-S = [int(input()) for _ in range(8)]
-S += S[:3]
-print(max([sum(S[i : i + 4]) for i in range(8)]))
+for i in range(int(input())):
+    N, X = map(int, input().split())
+    print(f"Case {i + 1}: {N % X}")
