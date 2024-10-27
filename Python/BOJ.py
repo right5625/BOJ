ecaa1da7cc84fdb0c992eee7b85b9d4d9d9aafe5
Jinho <right5625@naver.com>
2024-10-27 10:09:18 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,3 +1,6 @@
-for i in range(int(input())):
-    N, X = map(int, input().split())
-    print(f"Case {i + 1}: {N % X}")
+for _ in range(int(input())):
+    s1, s2 = input().split()
+    res = sum(ord(i) - ord(j) for i, j in zip(s1, s2))
+    print(
+        f"Swapping letters to make {s1} look like {s2} {'was FREE.' if res == 0 else f'earned {res} coins.' if res > 0 else f'cost {res * -1} coins.'}"
+    )
