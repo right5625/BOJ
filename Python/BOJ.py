838884fa0ff79cda424ffc109a1f2f6ff88d02b4
Jinho <right5625@naver.com>
2024-10-30 11:13:30 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,7 +1,6 @@
-lst1 = [0] * 26
-for i in input():
-    lst1[ord(i) - ord("a")] += 1
-lst2 = [0] * 26
-for i in input():
-    lst2[ord(i) - ord("a")] += 1
-print("".join(chr(ord("a") + i) * max(lst1[i], lst2[i]) for i in range(26)))
+n = int(input())
+res = inc = 0
+for i in range(1, n + 1):
+    inc += i
+    res += inc
+print(res)
