58c7a8f89957ebb53bc1f466c5c8a7664f5704ab
Jinho <right5625@naver.com>
2024-10-31 10:04:34 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,6 +1,19 @@
 n = int(input())
-res = inc = 0
-for i in range(1, n + 1):
-    inc += i
-    res += inc
-print(res)
+res = [0, 0]
+for i, j in zip(input(), input()):
+    if i == "R":
+        if j == "S":
+            res[0] += 1
+        elif j == "P":
+            res[1] += 1
+    elif i == "S":
+        if j == "R":
+            res[1] += 1
+        elif j == "P":
+            res[0] += 1
+    else:
+        if j == "R":
+            res[0] += 1
+        elif j == "S":
+            res[1] += 1
+print("victory" if res[0] > res[1] else "defeat")
