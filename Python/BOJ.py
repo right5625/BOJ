14f09a0e272f750599d443e268b12e7fe11def40
Jinho <right5625@naver.com>
2024-11-01 11:50:27 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,19 +1,8 @@
-n = int(input())
-res = [0, 0]
-for i, j in zip(input(), input()):
-    if i == "R":
-        if j == "S":
-            res[0] += 1
-        elif j == "P":
-            res[1] += 1
-    elif i == "S":
-        if j == "R":
-            res[1] += 1
-        elif j == "P":
-            res[0] += 1
-    else:
-        if j == "R":
-            res[0] += 1
-        elif j == "S":
-            res[1] += 1
-print("victory" if res[0] > res[1] else "defeat")
+pa = pb = 0
+res = "yes"
+for _ in range(int(input())):
+    a, b = map(int, input().split())
+    if a < pa or b < pb:
+        res = "no"
+    pa, pb = a, b
+print(res)
