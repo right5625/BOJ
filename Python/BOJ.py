a6e0c159c9cb7088589b9a5b9b85af2651006744
Jinho <right5625@naver.com>
2024-11-03 12:13:13 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,10 +1,7 @@
-A, B, N = map(int, input().split())
-res = float("inf")
-for _ in range(N):
-    c, n = map(int, input().split())
-    lst = list(map(int, input().split()))
-    a = lst.index(A) if A in lst else -1
-    b = lst.index(B) if B in lst else -1
-    if a != -1 and a < b:
-        res = min(res, c)
-print(res if res != float("inf") else -1)
+N = int(input())
+a = list(map(int, input().split()))
+x = 9001
+for i in range(N):
+    for j in range(i + 1, N):
+        x = min(a[j] - a[i], x)
+print("yes" if x >= 0 else "no")
