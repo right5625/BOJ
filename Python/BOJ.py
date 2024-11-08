ad31da3e9b79853f8fb20a047c45bbbdfc5d0dd5
Jinho <right5625@naver.com>
2024-11-08 14:20:00 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,6 +1,17 @@
-w = input()
-cnt = 0
-for i in set(w):
-    if w.count(i) % 2 == 1:
-        cnt += 1
-print(max(cnt - 1, 0))
+import string
+
+while True:
+    d, m, y = map(int, input().split())
+    if d == m == y == 0:
+        break
+    S = (d + m + y) % 25 + 1
+    print(
+        "".join(
+            (
+                string.ascii_lowercase[(ord(i) - ord("a") + (26 - S)) % 26]
+                if i in string.ascii_lowercase
+                else i
+            )
+            for i in input()
+        )
+    )
