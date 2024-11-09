8592733f071bedffef799d28fc9dc5875980a124
Jinho <right5625@naver.com>
2024-11-09 10:45:21 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,17 +1,12 @@
 import string
 
-while True:
-    d, m, y = map(int, input().split())
-    if d == m == y == 0:
-        break
-    S = (d + m + y) % 25 + 1
-    print(
-        "".join(
-            (
-                string.ascii_lowercase[(ord(i) - ord("a") + (26 - S)) % 26]
-                if i in string.ascii_lowercase
-                else i
-            )
-            for i in input()
-        )
-    )
+for _ in range(int(input())):
+    s = input()
+    for _ in range(int(input())):
+        w = input()
+        res = "YES"
+        for i in string.ascii_uppercase:
+            if s.count(i) < w.count(i):
+                res = "NO"
+                break
+        print(res)
