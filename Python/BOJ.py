009e49b55a8f1a5dd022532d0c6c4d08bbc00538
Jinho <right5625@naver.com>
2024-11-10 22:19:25 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,12 +1,37 @@
-import string
+import sys
+from collections import defaultdict
 
+input = lambda: sys.stdin.readline().rstrip()
+
+dic = {
+    "a": "2",
+    "b": "2",
+    "c": "2",
+    "d": "3",
+    "e": "3",
+    "f": "3",
+    "g": "4",
+    "h": "4",
+    "i": "4",
+    "j": "5",
+    "k": "5",
+    "l": "5",
+    "m": "6",
+    "n": "6",
+    "o": "6",
+    "p": "7",
+    "q": "7",
+    "r": "7",
+    "s": "7",
+    "t": "8",
+    "u": "8",
+    "v": "8",
+    "w": "9",
+    "x": "9",
+    "y": "9",
+    "z": "9",
+}
+cnt = defaultdict(int)
 for _ in range(int(input())):
-    s = input()
-    for _ in range(int(input())):
-        w = input()
-        res = "YES"
-        for i in string.ascii_uppercase:
-            if s.count(i) < w.count(i):
-                res = "NO"
-                break
-        print(res)
+    cnt["".join(dic[i] for i in input())] += 1
+print(cnt[input()])
