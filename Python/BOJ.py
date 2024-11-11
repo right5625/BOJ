bc7eb1bda165e5ea59ea5f5c1db969c2d578f7bd
Jinho <right5625@naver.com>
2024-11-11 11:34:03 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,37 +1,6 @@
-import sys
-from collections import defaultdict
-
-input = lambda: sys.stdin.readline().rstrip()
-
-dic = {
-    "a": "2",
-    "b": "2",
-    "c": "2",
-    "d": "3",
-    "e": "3",
-    "f": "3",
-    "g": "4",
-    "h": "4",
-    "i": "4",
-    "j": "5",
-    "k": "5",
-    "l": "5",
-    "m": "6",
-    "n": "6",
-    "o": "6",
-    "p": "7",
-    "q": "7",
-    "r": "7",
-    "s": "7",
-    "t": "8",
-    "u": "8",
-    "v": "8",
-    "w": "9",
-    "x": "9",
-    "y": "9",
-    "z": "9",
-}
-cnt = defaultdict(int)
-for _ in range(int(input())):
-    cnt["".join(dic[i] for i in input())] += 1
-print(cnt[input()])
+N = int(input())
+A = list(map(int, input().split()))
+res = [1 if A[0] else -1]
+for i in A[1:]:
+    res.append(res[-1] + 1 if i else res[-1] - 1)
+print(sum(res))
