0ef1dd5ea22d394fa6c8521136833c424d4d4a5d
Jinho <right5625@naver.com>
2024-11-12 11:32:16 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,6 +1,2 @@
 N = int(input())
-A = list(map(int, input().split()))
-res = [1 if A[0] else -1]
-for i in A[1:]:
-    res.append(res[-1] + 1 if i else res[-1] - 1)
-print(sum(res))
+print("No" if N > 100000 or N % 2024 != 0 else "Yes")
