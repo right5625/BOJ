fb001d2d91b38c64c99700991f527c0fbb9d35c2
Jinho <right5625@naver.com>
2024-11-14 11:13:59 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,1 +1,3 @@
-print("AKARAKA" + "RAKA" * (int(input()) - 1))
+a = sorted(list(map(int, input().split())))
+b = sorted(list(map(int, input().split())))
+print("YES" if a == b and a[0] ** 2 + a[1] ** 2 == a[2] ** 2 else "NO")
