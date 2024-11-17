bedb66c86df56006e3ea0dc1cf1e31573168aaa9
Jinho <right5625@naver.com>
2024-11-17 11:17:53 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,2 +1,12 @@
-P = [int(input()) for _ in range(int(input()))]
-print(min(P) - min(min(P), max(P) // 2))
+import sys
+
+input = lambda: sys.stdin.readline().rstrip()
+
+for _ in range(int(input())):
+    N = int(input())
+    res = ""
+    if N % 2 == 1:
+        res += "O"
+    if N**0.5 == int(N**0.5):
+        res += "S"
+    print(res if res else "EMPTY")
