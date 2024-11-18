a374388a9451a6d89ee0a6663a0eb447f9e093b8
Jinho <right5625@naver.com>
2024-11-18 11:47:30 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,12 +1,7 @@
-import sys
-
-input = lambda: sys.stdin.readline().rstrip()
-
-for _ in range(int(input())):
-    N = int(input())
-    res = ""
-    if N % 2 == 1:
-        res += "O"
-    if N**0.5 == int(N**0.5):
-        res += "S"
-    print(res if res else "EMPTY")
+lst1 = list(map(int, input().split()))
+lst2 = list(map(int, input().split()))
+res1, res2 = 0, 1.5
+for i, j in zip(range(6), [13, 7, 5, 3, 3, 2]):
+    res1 += lst1[i] * j
+    res2 += lst2[i] * j
+print("cocjr0208" if res1 > res2 else "ekwoo")
