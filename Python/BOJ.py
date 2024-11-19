d5dcfea295dd1f539d863d5b3e333e35fc03cbbd
Jinho <right5625@naver.com>
2024-11-19 10:44:23 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,7 +1,2 @@
-lst1 = list(map(int, input().split()))
-lst2 = list(map(int, input().split()))
-res1, res2 = 0, 1.5
-for i, j in zip(range(6), [13, 7, 5, 3, 3, 2]):
-    res1 += lst1[i] * j
-    res2 += lst2[i] * j
-print("cocjr0208" if res1 > res2 else "ekwoo")
+a, b, c = int(input()), int(input()), int(input())
+print("JAH" if a in [1, 3] and b in [6, 8] and c in [2, 5] else "EI")
