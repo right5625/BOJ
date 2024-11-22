2a039a75a05fe08acd1ac27947f2e809f3d91d7b
Jinho <right5625@naver.com>
2024-11-22 11:56:03 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,23 +1,7 @@
-N, K = map(int, input().split())
-dic = {i: [] for i in 'srp'}
-for i in input():
-    if len(dic[i]) < K:
-        dic[i].append(i)
-    else:
-        if i == 's':
-            if len(dic['r']) < K:
-                dic['r'].append(i)
-            else:
-                dic['p'].append(i)
-        elif i == 'r':
-            if len(dic['p']) < K:
-                dic['p'].append(i)
-            else:
-                dic['s'].append(i)
-        else:
-            if len(dic['s']) < K:
-                dic['s'].append(i)
-            else:
-                dic['r'].append(i)
-for i in dic.values():
-    print(''.join(i))
+for _ in range(int(input())):
+    S = list(input().split())
+    res = 0
+    for i in range(len(S)):
+        if S[i] in ['u', 'ur'] or (i < len(S) - 1 and ' '.join(S[i:i + 2]) in ['would of', 'should of']) or 'lol' in S[i]:
+            res += 10
+    print(res)
