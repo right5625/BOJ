d33db90fc476440e29e1adbfd94fd78e9ba5d3fd
Jinho <right5625@naver.com>
2024-11-23 12:03:38 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,7 +1,6 @@
+import math
+
 for _ in range(int(input())):
-    S = list(input().split())
-    res = 0
-    for i in range(len(S)):
-        if S[i] in ['u', 'ur'] or (i < len(S) - 1 and ' '.join(S[i:i + 2]) in ['would of', 'should of']) or 'lol' in S[i]:
-            res += 10
-    print(res)
+    a, b, c = map(int, input().split())
+    res = math.ceil((9000 - (a * 15 + b * 20 + c * 25)) / 40)
+    print(res if res <= 100 else 'impossible')
