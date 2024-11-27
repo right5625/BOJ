9d46dd990d424811b760a1e6ccc1915a114a2b10
Jinho <right5625@naver.com>
2024-11-27 11:36:56 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,1 +1,20 @@
-print(1 if int(input()) in set(i * j for i in range(2, 10) for j in range(1, 10)).union({1}) else 0)
+op = [list(input().split()) for _ in range(int(input()))]
+res = 0
+for i in range(1, 101):
+    num = i
+    for j in op:
+        if j[0] == 'ADD':
+            num += int(j[1])
+        elif j[0] == 'SUBTRACT':
+            num -= int(j[1])
+            if num < 0:
+                res += 1
+                break
+        elif j[0] == 'MULTIPLY':
+            num *= int(j[1])
+        else:
+            if num % int(j[1]) != 0:
+                res += 1
+                break
+            num /= int(j[1])
+print(res)
