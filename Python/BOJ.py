9bbe7b3253b7c410b338a1f442c3ec8b2922f721
Jinho <right5625@naver.com>
2024-11-28 12:37:30 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,20 +1,19 @@
-op = [list(input().split()) for _ in range(int(input()))]
-res = 0
-for i in range(1, 101):
-    num = i
-    for j in op:
-        if j[0] == 'ADD':
-            num += int(j[1])
-        elif j[0] == 'SUBTRACT':
-            num -= int(j[1])
-            if num < 0:
-                res += 1
-                break
-        elif j[0] == 'MULTIPLY':
-            num *= int(j[1])
-        else:
-            if num % int(j[1]) != 0:
-                res += 1
-                break
-            num /= int(j[1])
-print(res)
+n, k = map(int, input().split())
+floor = [0] * (k + 1)
+for _ in range(n):
+    f, t = input().split()
+    if t == 'SAFE':
+        for i in range(1, int(f) + 1):
+            floor[i] = 1
+    else:
+        for i in range(int(f), k + 1):
+            floor[i] = 2
+for i in range(2, k + 1):
+    if floor[i] % 2 == 0:
+        res1 = i
+        break
+for i in range(k - 1, 0, -1):
+    if floor[i] <= 1:
+        res2 = i
+        break
+print(res1, res2)
