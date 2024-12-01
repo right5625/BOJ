41df3e72c72cebb8b498f8dacd2c965db08b3e88
Jinho <right5625@naver.com>
2024-12-01 12:38:08 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,10 +1,6 @@
-n, a, b = map(int, input().split())
-res = [0, 0, 0]
-for i in range(1, n + 1):
-    if i % a == i % b == 0:
-        res[2] += 1
-    elif i % a == 0:
-        res[0] += 1
-    elif i % b == 0:
-        res[1] += 1
-print(*res)
+x, y = map(int, input().split())
+d = int(input())
+cm = x * 100 + y
+b = (cm - d * 2) // 4
+a = b + d
+print(f'{a // 100} {a % 100}\n{b // 100} {b % 100}')
