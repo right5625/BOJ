6b8a45252cfef9ccd415a35e5b7190b257e4a17e
Jinho <right5625@naver.com>
2024-12-02 11:41:52 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,6 +1,1 @@
-x, y = map(int, input().split())
-d = int(input())
-cm = x * 100 + y
-b = (cm - d * 2) // 4
-a = b + d
-print(f'{a // 100} {a % 100}\n{b // 100} {b % 100}')
+print('high speed rail' if int(input()) <= int(input()) else 'flight')
