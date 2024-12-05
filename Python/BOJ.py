066923bc29088dbe7d07896d30a8efff9ea06628
Jinho <right5625@naver.com>
2024-12-05 12:01:43 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,2 +1,6 @@
-k, n = map(int, input().split())
-print(n + 1, k * n + 1)
+for _ in range(int(input())):
+    a, b = map(int, input().split())
+    if a < b:
+        print('Inner circle line' if b - a < 22 else 'Outer circle line')
+    else:
+        print('Outer circle line' if a - b < 22 else 'Inner circle line')
