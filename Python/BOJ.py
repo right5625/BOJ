1cb7e808c6d9c76ca57c029884fae7edab51d093
Jinho <right5625@naver.com>
2024-12-09 11:50:43 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,10 +1,1 @@
-import sys
-input = lambda: sys.stdin.readline().rstrip()
-
-res = cur = 0
-for _ in range(int(input())):
-    a, b = map(int, input().split())
-    cur -= a
-    cur += b
-    res = max(res, cur)
-print(res)
+print('UOS'[(int(input()) - 1) % 3])
