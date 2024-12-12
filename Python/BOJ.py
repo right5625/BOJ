c139ea18036e5c3abf03842889231e5f4b8096e0
Jinho <right5625@naver.com>
2024-12-12 11:43:49 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,6 +1,17 @@
-from collections import defaultdict
+import sys
+input = lambda: sys.stdin.readline().rstrip()
 
-dic = defaultdict(int)
-for i in input():
-    dic[i] += 1
-print(sum(sorted(dic.values())[:-2]))
+for c in range(int(input())):
+    R, P, D = map(int, input().split())
+    r = D / P
+    dic = {}
+    for _ in range(R):
+        n, w, p = input().split()
+        w, p = float(w), float(p)
+        dic[n] = p
+        if p == 100:
+            r *= w
+    print(f'Recipe # {c + 1}')
+    for k, v in dic.items():
+        print(f'{k} {v * r / 100:.1f}')
+    print('----------------------------------------')
