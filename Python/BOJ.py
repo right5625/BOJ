5848ff068feae10d75bb1d37bdac95e85a08af66
Jinho <right5625@naver.com>
2024-12-14 11:10:35 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,11 +1,7 @@
-import string
-
-n, A, B = input().split()
-r = s = 0
-for i in range(int(n)):
-    if A[i] == B[i]:
-        r += 1
-for i in string.ascii_uppercase:
-    if i in A and i in B:
-        s += min(A.count(i), B.count(i))
-print(r, s - r)
+N, B = input().split()
+dic = {'A': [11, 11], 'K': [4, 4], 'Q': [3, 3], 'J': [20, 2], 'T': [10, 10], '9': [14, 0], '8': [0, 0], '7': [0, 0]}
+res = 0
+for _ in range(int(N) * 4):
+    S = input()
+    res += dic[S[0]][0] if S[1] == B else dic[S[0]][1]
+print(res)
