2b5843d9813bfa225eeeba6a90bb447f3a8b6732
Jinho <right5625@naver.com>
2024-12-15 10:26:39 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,7 +1,6 @@
-N, B = input().split()
-dic = {'A': [11, 11], 'K': [4, 4], 'Q': [3, 3], 'J': [20, 2], 'T': [10, 10], '9': [14, 0], '8': [0, 0], '7': [0, 0]}
+N = int(input())
 res = 0
-for _ in range(int(N) * 4):
-    S = input()
-    res += dic[S[0]][0] if S[1] == B else dic[S[0]][1]
+for i, j in zip(input(), input()):
+    if i == 'C' and i == j:
+        res += 1
 print(res)
