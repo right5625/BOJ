ef195347ec37bdfd37493154f23db376203c948c
Jinho <right5625@naver.com>
2024-12-16 12:21:07 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,6 +1,5 @@
-N = int(input())
-res = 0
-for i, j in zip(input(), input()):
-    if i == 'C' and i == j:
-        res += 1
-print(res)
+S = input()
+for k, v in {'socail': 'digital humanities', 'history': 'digital humanities', 'language': 'digital humanities', 'literacy': 'digital humanities', 'bigdata': 'public bigdata', 'public': 'public bigdata', 'society': 'public bigdata'}.items():
+    if k in S:
+        print(v)
+        break
