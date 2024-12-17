26a69647fab661bb042b1117b8cc3b714fc80d01
Jinho <right5625@naver.com>
2024-12-17 11:09:51 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,5 +1,2 @@
-S = input()
-for k, v in {'socail': 'digital humanities', 'history': 'digital humanities', 'language': 'digital humanities', 'literacy': 'digital humanities', 'bigdata': 'public bigdata', 'public': 'public bigdata', 'society': 'public bigdata'}.items():
-    if k in S:
-        print(v)
-        break
+a, b = map(int, input().split())
+print((a ** 2 + b ** 2) ** 0.5)
