61e1d35edf8901306477b75d4efc032b75d79cd8
Jinho <right5625@naver.com>
2024-12-18 10:16:39 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,2 +1,14 @@
-a, b = map(int, input().split())
-print((a ** 2 + b ** 2) ** 0.5)
+for i in range(int(input())):
+    N, K = map(int, input().split())
+    A = list(map(int, input().split()))
+    B = list(map(int, input().split()))
+    C = list(map(int, input().split()))
+    D = list(map(int, input().split()))
+    res = 0
+    for a in A:
+        for b in B:
+            for c in C:
+                for d in D:
+                    if a ^ b ^ c ^ d == K:
+                        res += 1
+    print(f'Case #{i + 1}: {res}')
