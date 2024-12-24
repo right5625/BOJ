3e95060e080302861f4fd773abd3cc0818e0c7bf
Jinho <right5625@naver.com>
2024-12-24 11:43:09 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,1 +1,7 @@
-print(int(input()) - 2024)
+_, X = map(int, input().split())
+res = 'YES'
+for _ in range(int(input())):
+    input()
+    if X not in list(map(int, input().split())):
+        res = 'NO'
+print(res)
