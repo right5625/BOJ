b5d535efa70e0aea9d815c8fb7ac199c95d553b4
Jinho <right5625@naver.com>
2024-12-31 12:46:49 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,3 +1,2 @@
-for i in range(int(input())):
-    S = input()
-    print(f'Case #{i + 1}: {S} is ruled by {'nobody' if S[-1].lower() == 'y' else ('a queen' if S[-1].lower() in 'aeiou' else 'a king')}.')
+input()
+print(*(set(input().split()) - set(input().split())))
