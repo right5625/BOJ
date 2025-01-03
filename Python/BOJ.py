9aa56683983002095774e365d0948c901f882835
Jinho <right5625@naver.com>
2025-01-03 11:45:27 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,2 +1,11 @@
-for num in range(int(input())):
-    print(f'Case #{num + 1}:', *input().split()[::-1])
+import string
+
+for _ in range(int(input())):
+    S = input()
+    flag = True
+    for i in S[:4] + S[5:]:
+        if not i in string.digits.replace('0', ''):
+            flag = False
+            break
+    if flag and len(set(S[:2])) == 1 and S[4] in string.ascii_uppercase:
+        print(S)
