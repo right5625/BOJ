d18b20ed063285f3b7693900c84a5035cb0f12e2
Jinho <right5625@naver.com>
2025-01-04 10:23:05 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,11 +1,9 @@
-import string
-
-for _ in range(int(input())):
-    S = input()
-    flag = True
-    for i in S[:4] + S[5:]:
-        if not i in string.digits.replace('0', ''):
-            flag = False
-            break
-    if flag and len(set(S[:2])) == 1 and S[4] in string.ascii_uppercase:
-        print(S)
+while True:
+    n = int(input())
+    if not n:
+        break
+    a = sorted(list(map(int, input().split())))
+    res = float('inf')
+    for i in range(n - 1):
+        res = min(res, a[i + 1] - a[i])
+    print(res)
