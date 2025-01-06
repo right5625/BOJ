9117b0fc8c194847aeb9ba48a49aa628215e1b24
Jinho <right5625@naver.com>
2025-01-06 12:19:11 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,10 +1,2 @@
-S = input()
-res, idx = [], 0
-while idx < len(S):
-    if len(set(S[idx : idx + 3])) == 3:
-        res.append('C')
-        idx += 3
-    else:
-        res.append({'R': 'S', 'B': 'K', 'L': 'H'}[S[idx]])
-        idx += 1
-print(''.join(res))
+S = [i for i in input() if i in 'aeiou']
+print('S' if S == S[::-1] else 'N')
