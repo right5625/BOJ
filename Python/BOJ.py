7b7ddf8bb3339ef68a066a8173c9b5f296939770
Jinho <right5625@naver.com>
2025-01-07 22:10:14 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,2 +1,2 @@
-S = [i for i in input() if i in 'aeiou']
-print('S' if S == S[::-1] else 'N')
+A, B, C = input().split()
+print(A + B + C) if A == B == C or (A == C and A != B) else print(A + B + C + A if B == C else A + B + C + B + A)
