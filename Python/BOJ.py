fd6c654c7bee1c2f88cc4c90e8e1804898313c2b
Jinho <right5625@naver.com>
2025-01-10 21:54:44 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,3 +1,2 @@
-N, R = map(int, input().split())
-res = set(i for i in range(1, N + 1)) - set(map(int, input().split()))
-print(*sorted(list(res)) if res else '*')
+S = input()
+print(len(S.replace(''.join(sorted(list(set(S)))), '', 1)))
