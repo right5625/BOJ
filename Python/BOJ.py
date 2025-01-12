1836ab1cc299503785f7af6eb96c88d7139746bf
Jinho <right5625@naver.com>
2025-01-12 09:34:37 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,8 +1,5 @@
 while True:
-    D, N = input().split()
-    if D == N == '0':
+    X1, Y1, X2, Y2 = map(int, input().split())
+    if X1 == Y1 == X2 == Y2 == 0:
         break
-    try:
-        print(int(N.replace(D, '')))
-    except:
-        print(0)
+    print(0) if X1 == X2 and Y1 == Y2 else print(1 if X1 == X2 or Y1 == Y2 or abs(X2 - X1) == abs(Y2 - Y1) else 2)
