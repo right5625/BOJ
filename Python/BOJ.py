c88e5087d48912f21b4020d17f29a116b073c654
Jinho <right5625@naver.com>
2025-01-15 19:27:45 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,5 +1,1 @@
-while True:
-    X1, Y1, X2, Y2 = map(int, input().split())
-    if X1 == Y1 == X2 == Y2 == 0:
-        break
-    print(0) if X1 == X2 and Y1 == Y2 else print(1 if X1 == X2 or Y1 == Y2 or abs(X2 - X1) == abs(Y2 - Y1) else 2)
+print(int(input()) * int(input()))
