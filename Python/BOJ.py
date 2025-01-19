b5f045e0b33e8b05eff8272e9a4a1fb369c411e9
Jinho <right5625@naver.com>
2025-01-19 10:08:09 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,2 +1,3 @@
-input()
-print(''.join({'J': 'O', 'O': 'I', 'I': 'J'}[i] for i in input()))
+P, Q = map(int, input().split())
+A, B = map(int, input().split())
+print(min(P, Q) * A + max(Q - P, 0) * B)
