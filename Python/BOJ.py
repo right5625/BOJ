20a88fa9050377a36109516778a4bd0a0a985950
Jinho <right5625@naver.com>
2025-01-21 22:58:50 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,1 +1,7 @@
-print(int(input()) // 10)
+b = int(input())
+result = 'Nothing'
+for k, v in {i: j for i, j in zip(['Watermelon', 'Pomegranates', 'Nuts'], [int(input()) for _ in range(3)])}.items():
+    if b >= v:
+        result = k
+        break
+print(result)
