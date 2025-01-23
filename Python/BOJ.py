3c72e4538080a4ed36833e803d3c52b16371772c
Jinho <right5625@naver.com>
2025-01-23 21:36:08 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,4 +1,21 @@
-N, M = map(int, input().split())
-A = list(map(int, input().split()))
-B = list(map(int, input().split()))
-print(sum((i + j) * max(i, j) for i in A for j in B))
+N = int(input())
+A = input()
+B = input()
+res = [0, 0]
+for i, j in zip(A, B):
+    if i == 'R':
+        if j == 'S':
+            res[0] += 1
+        elif j == 'P':
+            res[1] += 1
+    elif i == 'S':
+        if j == 'R':
+            res[1] += 1
+        elif j == 'P':
+            res[0] += 1
+    else:
+        if j == 'R':
+            res[0] += 1
+        elif j == 'S':
+            res[1] += 1
+print(*res)
