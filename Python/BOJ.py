664520e96e923f8f28baffee0bde2a1f3d940ab4
Jinho <right5625@naver.com>
2025-01-25 09:29:11 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,21 +1,4 @@
-N = int(input())
-A = input()
-B = input()
-res = [0, 0]
-for i, j in zip(A, B):
-    if i == 'R':
-        if j == 'S':
-            res[0] += 1
-        elif j == 'P':
-            res[1] += 1
-    elif i == 'S':
-        if j == 'R':
-            res[1] += 1
-        elif j == 'P':
-            res[0] += 1
-    else:
-        if j == 'R':
-            res[0] += 1
-        elif j == 'S':
-            res[1] += 1
-print(*res)
+import math
+
+n = int(input())
+print(sum(math.ceil(i / 2) for i in list(map(int, input().split()))))
