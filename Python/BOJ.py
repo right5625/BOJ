5a4b81c2edaabf3c9e5f740da0eb63fef1b91f95
Jinho <right5625@naver.com>
2025-01-27 10:59:22 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,4 +1,19 @@
 import math
 
-d, h = map(float, input().split())
-print(math.pi * 2 * (d / 2 + 5) * h + math.pi * (d / 2 + 5) ** 2)
+while True:
+    V, N, M = input().split()
+    if V == N == M == '0':
+        break
+    V, N, M = float(V), N.zfill(4), M.zfill(4)
+    res = V
+    if N[-4:] == M[-4:]:
+        res *= 3000
+    elif N[-3:] == M[-3:]:
+        res *= 500
+    elif N[-2:] == M[-2:]:
+        res *= 50
+    elif (math.ceil(int(N[-2:]) / 4) == math.ceil(int(M[-2:]) / 4)) or (N[-2:] in ['97', '98', '99', '00'] and M[-2] in ['97', '98', '99', '00']):
+        res *= 16
+    else:
+        res *= 0
+    print(f'{res:.2f}')
