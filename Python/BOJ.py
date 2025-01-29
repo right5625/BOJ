ae5a85a6569229eae22652ecf0db058e998512b2
Jinho <right5625@naver.com>
2025-01-29 10:31:38 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,2 +1,11 @@
-for _ in range(int(input())):
-    print(sum({'A': 1, 'B': 2, 'C': 0, 'D': 1, 'E': 0, 'F': 0, 'G': 0, 'H': 0, 'I': 0, 'J': 0, 'K': 0, 'L': 0, 'M': 0, 'N': 0, 'O': 1, 'P': 1, 'Q': 1, 'R': 1, 'S': 0, 'T': 0, 'U': 0, 'V': 0, 'W': 0, 'X': 0, 'Y': 0, 'Z': 0, ' ': 0}[i] for i in input()))
+while True:
+    s = input()
+    if s == '#':
+        break
+    res = 'not possible'
+    for i in range(len(s)):
+        t = s[:i] + s[i + 1:]
+        if t == t[::-1]:
+            res = t
+            break
+    print(res)
