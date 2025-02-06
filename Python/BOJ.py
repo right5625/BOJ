dedeef68b6202880cb0712117415580fb97b0bae
Jinho <right5625@naver.com>
2025-02-06 23:08:10 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,10 +1,16 @@
+import string
+
 N = int(input())
-A = list(map(int, input().split()))
-B = list(map(int, input().split()))
-a = b = res = 0
-for i in range(N):
-    a += A[i]
-    b += B[i]
-    if a == b:
-        res = i + 1
-print(res)
+S = input().replace('.', '-').replace('?', '-').replace('!', '-').split('-')
+for i in S[:-1]:
+    cnt = 0
+    for j in i.strip().split():
+        if j[0] in string.ascii_uppercase:
+            flag = True
+            for k in j[1:]:
+                if k not in string.ascii_lowercase:
+                    flag = False
+                    break
+            if flag:
+                cnt += 1
+    print(cnt)
