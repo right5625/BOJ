b82daf8894dca0a0bd554eb6c63be38e791a1a10
Jinho <right5625@naver.com>
2025-02-10 18:40:34 +0900

    Commit

--- Python/BOJ.py
+++ Python/BOJ.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 def dfs(r, c, t):
     global res
     if r == c == N:
@@ -46,16 +45,3 @@ vst[1][1] = vst[1][2] = True
 res = 0
 dfs(1, 2, 0)
 print(res)
-=======
-import sys, string
-input = lambda: sys.stdin.readline().rstrip()
-
-s = string.digits + string.ascii_lowercase
-def convert(n, b):
-    q, r = divmod(n, b)
-    return s[r] if q == 0 else convert(q, b) + s[r]
-
-for _ in range(int(input())):
-    K, b, n = map(int, input().split())
-    print(K, sum(s.index(i) ** 2 for i in convert(n, b)))
->>>>>>> 4fc83c260c222b8485c24defca145ab85a6354ec
